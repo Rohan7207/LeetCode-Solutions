3f64f41eddb505239ba6fd5da81de0e28a304f34
Rohan Figredo <rohanfigredo@gmail.com>
2026-04-10 21:37:43 +0530

    Time: 7 ms (36.69%), Space: 45.3 MB (82.68%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        //O(n) and O(1)
+        // int left = 0;
+        // int n = s.length();
+        // Set<Character> charset = new HashSet<>();
+        // int maxlength = 0;
+
+        // for(int right = 0; right < n; right++){
+        //     if(!charset.contains(s.charAt(right))){
+        //         charset.add(s.charAt(right));
+        //         maxlength = Math.max(maxlength, right - left + 1);
+        //     }else{
+        //         while(charset.contains(s.charAt(right))){
+        //             charset.remove(s.charAt(left));
+        //             left++;
+        //         }
+        //         charset.add(s.charAt(right));
+        //     }
+        // }
+
+        // return maxlength;
+
+        //O(n) and O(1)
+        //Sliding window and hashing 
+
+        int maxLength = 0;
+        int left = 0;
+        Map<Character, Integer> count = new HashMap<>();
+
+        for(int right = 0; right < s.length(); right++){
+            char c = s.charAt(right);
+            count.put(c, count.getOrDefault(c, 0) + 1);
+
+            /*This while loop adjusts the left pointer as long as there are repeating characters in the current substring.
+It decreases the count of the character at index left and increments left by 1 until there are no repeating character */
+            while(count.get(c) > 1){
+                char leftChar = s.charAt(left);
+                count.put(leftChar, count.get(leftChar) - 1);
+                left++;
+            }
+
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+}
+
