0508ae3fd02cea57940a00727b5a0dad29024937
Rohan Figredo <rohanfigredo@gmail.com>
2026-04-13 19:09:32 +0530

    Time: 0 ms (100%), Space: 44.8 MB (84.16%) - LeetHub

---
+++ 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int getMinDistance(int[] nums, int target, int start) {
+        // // Time = O(n) and Space = O(1)
+        // int min = Integer.MAX_VALUE;
+
+        // for(int i = 0; i < nums.length; i++) {
+        //     if(nums[i] == target) {
+        //         min = Math.min(min, Math.abs(start - i));
+        //     }
+        // }
+
+        // return min;
+
+        // Solution using the start where time = O(n) and space = O(1)
+
+        if(nums[start] == target) return 0;
+
+        int n = nums.length;
+        int d = 1;  //It is no.of steps of start moving outward or inward
+
+        while(true) {
+            if(start - d >= 0 && nums[start - d] == target) return d;
+            if(start + d < n && nums[start + d] == target) return d;
+
+            d++; 
+        }
+    }
+}
