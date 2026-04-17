0e2e1d3049db1d7562e4e0a913995cebc6a8fd11
Rohan Figredo <rohanfigredo@gmail.com>
2026-04-17 18:45:41 +0530

    Time: 3 ms (78.63%), Space: 46.4 MB (68.43%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,31 +1,30 @@
 class Solution {
+    int getValue(char ch) {
+        if(ch == 'I') return 1;
+        else if(ch == 'V') return 5;
+        else if(ch == 'X') return 10;
+        else if(ch == 'L') return 50;
+        else if(ch == 'C') return 100;
+        else if(ch == 'D') return 500;
+        return 1000;
+    }
+
     public int romanToInt(String s) {
         int num = 0;
-        for(int i = 0; i < s.length(); i++){
-            char currchar = s.charAt(i);
 
-            if(i < s.length() - 1){
-                char nextchar = s.charAt(i + 1);
-            
-                if(currchar == 'I' && (nextchar == 'V' || nextchar == 'X')){
-                    num -= 1;
-                    continue;
-                }else if(currchar == 'X' && (nextchar == 'L' || nextchar == 'C')){
-                    num -= 10;
-                    continue;
-                }else if(currchar == 'C' && (nextchar == 'D' || nextchar == 'M')){
-                    num -= 100;
+        for(int i = 0; i < s.length(); i++) {
+            int curr = getValue(s.charAt(i));
+
+            if(i < s.length() - 1) {
+                int next = getValue(s.charAt(i + 1));
+
+                if(curr < next) {
+                    num -= curr;
                     continue;
                 }
             }
-        
-            if(currchar == 'I') num += 1;
-            else if(currchar == 'V') num += 5;
-            else if(currchar == 'X') num += 10;
-            else if(currchar == 'L') num += 50;
-            else if(currchar == 'C') num += 100;
-            else if(currchar == 'D') num += 500;
-            else if(currchar == 'M') num += 1000;
+
+            num += curr;
         }
 
         return num;
