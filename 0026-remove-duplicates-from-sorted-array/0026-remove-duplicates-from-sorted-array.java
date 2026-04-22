91cf9c126febc4c5e70137234316830ed801c5e2
Rohan Figredo <rohanfigredo@gmail.com>
2026-04-22 19:17:10 +0530

    Time: 1 ms (76.85%), Space: 47 MB (11.03%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,19 +1,3 @@
-// Problem: Remove Duplicates from Sorted Array
-// Link: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
-// Difficulty: Easy
-
-// Approach:
-// Use two pointers to remove duplicates in-place.
-// Keep one pointer to track the position of the next unique element.
-// Traverse the array using another pointer.
-// If the current element is different from the last unique element,
-//     place it at the next unique position and move the pointer forward.
-// Return the count of unique elements.
-
-// Time Complexity: O(n)
-// Space Complexity: O(1)
-
-
 class Solution {
     public int removeDuplicates(int[] nums) {
         int j = 1;
