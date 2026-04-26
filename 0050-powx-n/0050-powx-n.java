e27c42289a265bc093dbe6e890f26196cfe5942d
Rohan Figredo <rohanfigredo@gmail.com>
2026-04-26 19:25:01 +0530

    Time: 0 ms (100%), Space: 47.7 MB (79.21%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,19 +1,21 @@
 class Solution {
-    public double help(double x,long n){
-        if(n==0) return 1;
-        double half=help(x,n/2);
-        if(n%2==0){
-            return half*half;
-        }else{
-            return half*half*x;
+    public double help(double x, long n) {
+        if (n == 0)
+            return 1;
+        double half = help(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
         }
     }
+
     public double myPow(double x, int n) {
-        long N=n; 
-        if(n<0){
-            x=1/x;
-            N=-N;
+        long N = n;
+        if (n < 0) {
+            x = 1 / x;
+            N = -N;
         }
-        return help(x,N);
+        return help(x, N);
     }
 }
