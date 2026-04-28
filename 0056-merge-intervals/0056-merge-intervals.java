5f668dbcc39d9c639067b1b34a327e3312919ba5
Rohan Figredo <rohanfigredo@gmail.com>
2026-04-28 19:22:25 +0530

    Refactor merge intervals solution and cleanup
    Refactor merge intervals logic with improved comments and remove unused code.

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,3 +1,24 @@
+// Problem: Insert Interval
+// Link: https://leetcode.com/problems/insert-interval/
+// Difficulty: Medium
+
+// Approach:
+// Sort the intervals based on starting values.
+// If starting values are equal, sort by ending values.
+// Use pointer l to track the last merged interval position.
+// Traverse the intervals starting from index 1:
+//     - If current interval does not overlap with
+//       the last merged interval:
+//           move l forward and store the current interval.
+//     - Otherwise, merge intervals by updating
+//       the ending value with the maximum end.
+// Return only the merged portion of the array
+// using Arrays.copyOfRange().
+
+// Time Complexity: O(n log n)
+// Space Complexity: O(1) (excluding sorting space)
+
+
 class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> a[0] == b[0] ? Integer.compare(a[1], b[1]) : Integer.compare(a[0], b[0]));
@@ -16,28 +37,3 @@ class Solution {
         return Arrays.copyOfRange(intervals, 0, l + 1);
     }
 }
-
-/*
-We sort the given input and check for 2nd list 1st element is lesser than previous 2nd
-element then merge that two intervals based on min and max
-Consider [[2,5],[4,6],[7,9],[11,15],[13,16]] that is after sorting
-1. since 4<5, there is overlap so min of 2 and 4 and max of 5 and 6 i.e. is 2 6 is added to res
-2. 7>6, there is no overlap
-3. 11>9, same here
-4. 13<15, there is overlap gives 11 16 added to res
-
-Time = O(nlogn) and Space =  O(n) 
-
-    Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        LinkedList<int[]> res = new LinkedList<>();
-
-        for (int[] interval : intervals) {
-            if (res.isEmpty() || res.getLast()[1] < interval[0]) {
-                res.add(interval);
-            } else {
-                res.getLast()[1] = Math.max(res.getLast()[1], interval[1]);
-            }
-        }
-
-        return res.toArray(new int[res.size()][]);
-*/
