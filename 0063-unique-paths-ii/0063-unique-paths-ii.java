c5c37903a096e7f0a92d00dbd668e511fb73b6e7
Rohan Figredo <rohanfigredo@gmail.com>
2026-04-29 19:20:21 +0530

    Time: 0 ms (100%), Space: 43.8 MB (11.5%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,23 +1,21 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        //time = O(r * c) and space = O(c)
-
         int rows = obstacleGrid.length;
         int cols = obstacleGrid[0].length;
 
-        if(obstacleGrid == null || obstacleGrid[0][0] == 1){
+        if (obstacleGrid == null || obstacleGrid[0][0] == 1) {
             return 0;
         }
 
         int[] dp = new int[cols];
-        dp[0] = 1;   //Set dp[0] to 1, representing the number of ways to reach the starting cell (0, 0).
+        dp[0] = 1; 
 
-        for(int r = 0; r < rows; r++){
-            for(int c = 0; c < cols; c++){
-                if(obstacleGrid[r][c] == 1){
-                    dp[c] = 0;     //if there is obstacle set it 0, indicating that there is no path to reach that cell
-                }else{
-                    if(c > 0){   //it should not be zero since we are adding the left value to it
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (obstacleGrid[r][c] == 1) {
+                    dp[c] = 0; 
+                } else {
+                    if (c > 0) { 
                         dp[c] += dp[c - 1];
                     }
                 }
@@ -27,3 +25,13 @@ class Solution {
         return dp[cols - 1];
     }
 }
+
+//time = O(r * c) and space = O(c)
+
+/*
+    Set dp[0] to 1, representing the number of ways to reach the starting cell (0, 0).
+    
+    If there is obstacle set it 0, indicating that there is no path to reach that cell
+    
+    It should not be zero since we are adding the left value to it
+*/
