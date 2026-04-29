d4210e3c8e5b1faa93884da1299ffb7a296fcf43
Rohan Figredo <rohanfigredo@gmail.com>
2026-04-29 19:35:56 +0530

    Add dynamic programming solution for unique paths
    Implemented a dynamic programming solution for calculating unique paths in a grid using a 1D array to optimize space.

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,3 +1,26 @@
+// Problem: Unique Paths
+// Link: https://leetcode.com/problems/unique-paths/
+// Difficulty: Medium
+
+// Approach:
+// Use dynamic programming with a 1D array to optimize space.
+// Create a dp array where each element stores
+//     the number of ways to reach the current column.
+// Initialize all values as 1 because the first row
+//     can only be reached by moving right.
+// Traverse the grid row by row starting from row 1:
+//     - Update each cell using:
+//           dp[j] = dp[j] + dp[j - 1]
+//       where:
+//           dp[j]     -> paths from top
+//           dp[j - 1] -> paths from left
+// The last element of the array contains
+//     the total unique paths to the destination.
+
+// Time Complexity: O(m * n)
+// Space Complexity: O(n)
+
+
 class Solution {
     public int uniquePaths(int m, int n) {
         int[] dp = new int[n];
@@ -13,5 +36,3 @@ class Solution {
         return dp[n - 1];
     }
 }
-
-// “I optimize the DP solution by using a single array where each element stores paths from the top and left for the current row.”
