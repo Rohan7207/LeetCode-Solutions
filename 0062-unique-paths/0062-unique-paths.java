cbb9e297a90a732e4931f878698618b2302a8641
Rohan Figredo <rohanfigredo@gmail.com>
2026-04-29 19:07:36 +0530

    Time: 1 ms (13.49%), Space: 42 MB (81.62%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -2,16 +2,18 @@ class Solution {
     public int uniquePaths(int m, int n) {
         int[][] grid = new int[m][n];
 
-        for(int[] row : grid){
+        for (int[] row : grid) {
             Arrays.fill(row, 1);
         }
 
-        for(int i = 1; i < m; i++){
-            for(int j = 1; j < n; j++){
-                grid[i][j] = grid[i - 1][j] + grid[i][j - 1];  //Checks the previous left and above 
-            }                                                  //since we have travel right and down 
-        }                                                      //to reach that position
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                grid[i][j] = grid[i - 1][j] + grid[i][j - 1]; 
+            } 
+        } 
 
         return grid[m - 1][n - 1];
     }
 }
+
+// In line no. 11 Checks the previous left and above since we have to  travel right and down to reach that position
