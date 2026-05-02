54448fc1a1387a74f2f4e2380ca096bd49c962fd
Rohan Figredo <rohanfigredo@gmail.com>
2026-05-02 18:52:05 +0530

    Time: 2 ms (99.61%), Space: 45.1 MB (41.68%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums); // Sort array to handle duplicates
+        helper(0, nums, new ArrayList<>(), res);
+        return res;
+    }
+
+    private void helper(int start, int[] nums, List<Integer> current, List<List<Integer>> res){
+        res.add(new ArrayList<>(current));
+        for(int i = start; i < nums.length; i++){
+        //Skip the duplicate
+        if(i > start && nums[i] == nums[i - 1]){
+            continue;
+        }
+
+        current.add(nums[i]);
+        helper(i + 1, nums, current, res);
+        current.remove(current.size() - 1);
+        }
+    }
+}
