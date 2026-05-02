a2fce9e22ad02b13309eddd73377d1ac7be3a445
Rohan Figredo <rohanfigredo@gmail.com>
2026-05-02 18:52:52 +0530

    Time: 3 ms (56.25%), Space: 45 MB (76.21%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -6,17 +6,17 @@ class Solution {
         return res;
     }
 
-    private void helper(int start, int[] nums, List<Integer> current, List<List<Integer>> res){
+    private void helper(int start, int[] nums, List<Integer> current, List<List<Integer>> res) {
         res.add(new ArrayList<>(current));
-        for(int i = start; i < nums.length; i++){
-        //Skip the duplicate
-        if(i > start && nums[i] == nums[i - 1]){
-            continue;
-        }
+        for (int i = start; i < nums.length; i++) {
+            //Skip the duplicate
+            if (i > start && nums[i] == nums[i - 1]) {
+                continue;
+            }
 
-        current.add(nums[i]);
-        helper(i + 1, nums, current, res);
-        current.remove(current.size() - 1);
+            current.add(nums[i]);
+            helper(i + 1, nums, current, res);
+            current.remove(current.size() - 1);
         }
     }
 }
