a6275868c0601ed9ce65d766fd6fa593a815baf3
Rohan Figredo <rohanfigredo@gmail.com>
2026-05-15 19:14:05 +0530

    Time: 0 ms (100%), Space: 44.5 MB (86.29%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.java
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.java
@@ -1,6 +1,30 @@
 class Solution {
     public boolean isGood(int[] nums) {
-        Set<Integer> seen = new HashSet<>();
+        int n = 0;
+
+        for(int num : nums) {
+            n = Math.max(n, num);
+        }
+
+        if(nums.length != n + 1) return false;
+
+        int[] freq = new int[201];
+
+        for(int num : nums) {
+            freq[num]++;
+        }
+
+        // Every number from 1 to n-1 appears exactly once
+        for(int i = 1; i < n; i++) {
+            if(freq[i] != 1) return false;
+        }
+
+        return freq[n] == 2;
+    }
+}
+
+/*
+    Set<Integer> seen = new HashSet<>();
         int n = nums.length - 1;
         boolean flag = false;
 
@@ -17,5 +41,4 @@ class Solution {
         }
 
         return true;
-    }
-}
+*/
