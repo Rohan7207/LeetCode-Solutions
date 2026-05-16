490f15f58363ba71a3423a60f501efd8490c0005
Rohan Figredo <rohanfigredo@gmail.com>
2026-05-16 18:58:22 +0530

    Add problem details and approach for findMin method
    Added problem description, approach, and complexity analysis for the 'Find Minimum in Rotated Sorted Array II'.

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.java
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.java
@@ -1,3 +1,27 @@
+// Problem: Find Minimum in Rotated Sorted Array II
+// Link: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/?envType=daily-question&envId=2026-05-16
+// Difficulty: Hard
+
+// Approach:
+// Use Binary Search to find the minimum element
+// in the rotated sorted array containing duplicates.
+// Compare mid element with high element:
+//     - If nums[mid] > nums[h],
+//       minimum lies in right half.
+//     - If nums[mid] < nums[h],
+//       minimum lies in left half including mid.
+//     - If nums[mid] == nums[h],
+//       duplicates prevent identifying the sorted half,
+//       so reduce search space by decrementing h.
+// Continue until low and high meet.
+// Return nums[l] as minimum element.
+
+// Time Complexity:
+//     Average: O(log n)
+//     Worst Case: O(n)
+// Space Complexity: O(1)
+
+
 class Solution {
     public int findMin(int[] nums) {
         int l = 0;
