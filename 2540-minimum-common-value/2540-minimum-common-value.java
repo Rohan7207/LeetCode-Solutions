c16f6f9ed63ae66068a4ec84b118ef72aeee2576
Rohan Figredo <rohanfigredo@gmail.com>
2026-05-19 19:38:04 +0530

    Add comments for Minimum Common Value problem
    Added problem description and approach comments for clarity.

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -1,3 +1,26 @@
+// Problem: Minimum Common Value
+// Link: https://leetcode.com/problems/minimum-common-value/?envType=daily-question&envId=2026-05-19
+// Difficulty: Easy
+
+// Approach:
+// Since both arrays are sorted,
+// use two pointers to traverse them.
+// Compare current elements of both arrays:
+//     - If nums1[i] < nums2[j],
+//       move pointer i forward because
+//       smaller element cannot match later.
+//     - If nums2[j] < nums1[i],
+//       move pointer j forward.
+//     - If both elements are equal,
+//       return that value immediately
+//       because it is the smallest common value.
+// If traversal finishes without match,
+// return -1.
+
+// Time Complexity: O(n + m)
+// Space Complexity: O(1)
+
+
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
         int i = 0, j = 0;
