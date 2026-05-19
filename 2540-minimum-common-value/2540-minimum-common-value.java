519fa00ea202ee7ab649395bca34d34787e27aa1
Rohan Figredo <rohanfigredo@gmail.com>
2026-05-19 19:34:33 +0530

    Time: 2 ms (88.98%), Space: 80.3 MB (79.53%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -3,12 +3,12 @@ class Solution {
         int i = 0, j = 0;
 
         while (i < nums1.length && j < nums2.length) {
-            if(nums1[i] < nums2[j]) {
-                i++;
-            } else if(nums2[j] < nums1[i]) {
-                j++;
-            } else {
+            if (nums1[i] == nums2[j]) {
                 return nums1[i];
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
             }
         }
 
