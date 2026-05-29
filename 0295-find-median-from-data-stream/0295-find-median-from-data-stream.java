e7a2abf27ae14339946c57715d9f791c6129ed74
Rohan Figredo <rohanfigredo@gmail.com>
2026-05-29 21:44:14 +0530

    Time: 120 ms (89.07%), Space: 111.5 MB (36.93%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,25 +1,33 @@
 class MedianFinder {
-    PriorityQueue<Integer> minHeap;
-    PriorityQueue<Integer> maxHeap;
+    PriorityQueue<Integer> left;
+    PriorityQueue<Integer> right;
 
     public MedianFinder() {
-        minHeap = new PriorityQueue<>();
-        maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        left = new PriorityQueue<>((a, b) -> b - a);
+        right = new PriorityQueue<>();
     }
 
-    //adds num to data stream
     public void addNum(int num) {
-        maxHeap.offer(num); //add to max heap
+        if (left.isEmpty() || num <= left.peek()) {
+            left.add(num);
+        } else
+            right.add(num);
 
-        minHeap.offer(maxHeap.poll());
-
-        if (maxHeap.size() < minHeap.size()) {
-            maxHeap.offer(minHeap.poll());
+        if (left.size() > right.size() + 1) {
+            right.add(left.poll());
+        }
+        if (right.size() > left.size() + 1) {
+            left.add(right.poll());
         }
     }
 
     public double findMedian() {
-        return maxHeap.size() > minHeap.size() ? maxHeap.peek() : (maxHeap.peek() + minHeap.peek()) * 0.5;
+        if (left.size() > right.size())
+            return left.peek();
+        if (right.size() > left.size())
+            return right.peek();
+
+        return (left.peek() + right.peek()) / 2.0;
     }
 }
 
@@ -29,3 +37,28 @@ class MedianFinder {
  * obj.addNum(num);
  * double param_2 = obj.findMedian();
  */
+
+/*
+   PriorityQueue<Integer> minHeap;
+   PriorityQueue<Integer> maxHeap;
+
+   public MedianFinder() {
+       minHeap = new PriorityQueue<>();
+       maxHeap = new PriorityQueue<>((a, b) -> b - a);
+   }
+
+   //adds num to data stream
+   public void addNum(int num) {
+       maxHeap.offer(num); //add to max heap
+
+       minHeap.offer(maxHeap.poll());
+
+       if (maxHeap.size() < minHeap.size()) {
+           maxHeap.offer(minHeap.poll());
+       }
+   }
+
+   public double findMedian() {
+       return maxHeap.size() > minHeap.size() ? maxHeap.peek() : (maxHeap.peek() + minHeap.peek()) * 0.5;
+   }
+*/
