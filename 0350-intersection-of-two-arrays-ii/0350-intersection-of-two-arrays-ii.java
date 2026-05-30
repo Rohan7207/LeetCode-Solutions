87cb30f47f359b7156080348f6f5893cc87b3ef6
Rohan Figredo <rohanfigredo@gmail.com>
2026-05-30 22:48:42 +0530

    Refactor intersection logic and remove commented code

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,15 +1,35 @@
+// Problem : Intersection of Two Arrays II
+// Link : https://leetcode.com/problems/intersection-of-two-arrays-ii/
+// Difficulty : Easy
+
+// Approach:
+// Store frequency of nums1 elements
+// in a HashMap.
+// Traverse nums2:
+//     - If current number exists in map
+//       and frequency > 0,
+//       add it to answer.
+//     - Decrease frequency.
+// This ensures duplicates are counted
+// exactly as many times as they appear
+// in both arrays.
+
+// Time Complexity: O(n + m)
+// Space Complexity: O(min(n,m))
+
+
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         Map<Integer, Integer> map = new HashMap<>();
 
-        for(int num : nums1) {
+        for (int num : nums1) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
         ArrayList<Integer> list = new ArrayList<>();
 
-        for(int num : nums2) {
-            if(map.containsKey(num) && map.get(num) > 0) {
+        for (int num : nums2) {
+            if (map.containsKey(num) && map.get(num) > 0) {
                 list.add(num);
                 map.put(num, map.get(num) - 1);
             }
@@ -17,35 +37,10 @@ class Solution {
 
         int[] ans = new int[list.size()];
 
-        for(int i = 0; i < list.size(); i++) {
+        for (int i = 0; i < list.size(); i++) {
             ans[i] = list.get(i);
         }
 
         return ans;
     }
 }
-
-/*
-    Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        int n = nums1.length, m = nums2.length;
-        int i = 0, j = 0;
-        List<Integer> list = new ArrayList<>();
-        while(i < n && j < m){
-            int a = nums1[i], b= nums2[j];
-            if(a == b){
-                list.add(a);
-                i++;
-                j++;
-            }else if(a < b){
-                i++;
-            }else{
-                j++;
-            }
-        }
-        
-        int[] arr = new int[list.size()];
-        for(int k = 0; k < list.size();k++) 
-          arr[k] = list.get(k);
-        return arr; 
-*/
