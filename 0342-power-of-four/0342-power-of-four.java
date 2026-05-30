02fc95c885a183dba26db993b81a861f5cbf840d
Rohan Figredo <rohanfigredo@gmail.com>
2026-05-30 22:32:02 +0530

    Time: 1 ms (90.91%), Space: 42.3 MB (91.99%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,14 +1,11 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
         if(n <= 0) return false;
-        if(n == 1) return true;
 
-        while(n > 1) {
-            if(n % 4 != 0) return false;
-
-            n /= 4; // Floor division which rounds of answer
+        while(n % 4 == 0) {
+            n /= 4; 
         }
 
-        return true;
+        return n == 1;
     }
 }
