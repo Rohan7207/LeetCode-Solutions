e1b6a1107020db0b3885cb2813fe947dacaa024d
Rohan Figredo <rohanfigredo@gmail.com>
2026-05-30 22:21:22 +0530

    Update 0338-counting-bits.java

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,16 +1,25 @@
+// Problem : Counting Bits
+// Link : https://leetcode.com/problems/counting-bits/
+// Difficulty : Easy
+
+// Approach:
+// Use Dynamic Programming.
+// Let dp[i] represent the number of
+// set bits (1s) in i.
+// Use Brian Kernighan's observation:
+//     i & (i - 1)
+// removes the rightmost set bit.
+// Therefore:
+//     dp[i] = dp[i & (i - 1)] + 1
+// Since (i & (i - 1)) is always smaller
+// than i, its answer is already computed.
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+
 class Solution {
     public int[] countBits(int n) {
-        //Approach 1
-        //We do & with n and n-1 and return when n=0 and return count
-        //Time with O(n) and O(n)
-
-        //Approach 2 with O(n) and O(1)
-        /*Ans for 9= A(n&n-1)+1
-                    =A(9&8)+1
-                    =A(8)+1
-        */
-
-        // ans = {0, 1, 1, 2, 1, 2}
         int[] ans = new int[n + 1];
         ans[0] = 0;
 
