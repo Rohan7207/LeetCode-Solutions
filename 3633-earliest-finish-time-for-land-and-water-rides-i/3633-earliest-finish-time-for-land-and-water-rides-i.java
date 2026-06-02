0be7e42aa9048ecd47a50b73eef526fc8bba552c
Rohan Figredo <rohanfigredo@gmail.com>
2026-06-02 19:29:50 +0530

    Update 3633-earliest-finish-time-for-land-and-water-rides-i.java

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.java
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.java
@@ -1,3 +1,27 @@
+// Problem: Earliest Finish Time for Land and Water Rides I
+// Link: https://leetcode.com/problems/earliest-finish-time-for-land-and-water-rides-i/?envType=daily-question&envId=2026-06-02
+// Difficulty: Easy
+
+// Approach:
+// Try every combination of one land ride
+// and one water ride.
+//
+// For each pair, calculate two possibilities:
+//
+//     1. Land ride first, then water ride.
+//     2. Water ride first, then land ride.
+//
+// When taking the second ride,
+// if it has not started yet, wait until
+// its start time using max().
+//
+// Update the answer with the minimum
+// finish time among all possibilities.
+
+// Time Complexity: O(L * W)
+// Space Complexity: O(1)
+
+
 class Solution {
     public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
         int ans = Integer.MAX_VALUE;
