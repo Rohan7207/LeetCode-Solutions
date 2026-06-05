aea25a4626781161fd5eaad1a0729e94a39a70e8
Rohan Figredo <rohanfigredo@gmail.com>
2026-06-05 19:32:11 +0530

    Add logic to count non-overlapping intervals
    Implemented logic to count non-overlapping intervals by sorting and comparing end times.

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -1,3 +1,36 @@
+// Problem: Non -overlapping Intervals
+// Link: https://leetcode.com/problems/non-overlapping-intervals/
+// Difficulty: Medium
+
+// Approach:
+// Sort intervals according to their
+// ending times.
+// Keep track of the end time of the
+// last selected non-overlapping interval
+// using 'prev'.
+// Traverse remaining intervals.
+// If:
+//     prev > currentStart
+// then current interval overlaps with
+// the previously selected interval.
+//
+// To minimize removals, keep the interval
+// with the smaller ending time
+// (already ensured by sorting).
+//
+// Therefore:
+//     remove current interval
+//     count++
+// Otherwise:
+//     no overlap
+//     keep current interval
+//     update prev to current end
+// Finally return count.
+
+// Time Complexity: O(n log n)
+// Space Complexity: O(1)
+
+
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
@@ -5,11 +38,9 @@ class Solution {
         int count = 0;
 
         for (int i = 1; i < intervals.length; i++) {
-            //Overlapping case
             if (prev > intervals[i][0]) {
                 count++;
             } else {
-                //Non overlapping assign prev to next interval
                 prev = intervals[i][1];
             }
         }
