2801e1b2335311f2fb7cfa56fd70a9a554e3f79a
Rohan Figredo <rohanfigredo@gmail.com>
2026-06-08 21:46:30 +0530

    Time: 5 ms (73.56%), Space: 159.9 MB (49.83%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,6 +1,35 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
         int[] ans = new int[nums.length];
+        int lessCount = 0;
+        int equalCount = 0;
+
+        for(int num : nums) {
+            if(num < pivot) lessCount++;
+            else if(num == pivot) equalCount++;
+        }
+
+        // Reserve the positions in array for less, equal and greater
+        int lessIndex = 0;
+        int equalIndex = lessCount;
+        int greaterIndex = lessCount + equalCount;
+
+        for(int num : nums) {
+            if(num < pivot) {
+                ans[lessIndex++] = num;
+            } else if(num == pivot) {
+                ans[equalIndex++] = num;
+            } else {
+                ans[greaterIndex++] = num;
+            }
+        }
+
+        return ans;
+    }
+}
+
+/*
+    int[] ans = new int[nums.length];
         int index = 0;
 
         for(int num : nums) {
@@ -16,5 +45,4 @@ class Solution {
         }
 
         return ans;
-    }
-}
+*/
