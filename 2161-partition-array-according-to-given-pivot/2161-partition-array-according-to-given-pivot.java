0c87755eed9d715d83013b4268670a5653dc5d8c
Rohan Figredo <rohanfigredo@gmail.com>
2026-06-08 21:47:47 +0530

    Time: 5 ms (73.56%), Space: 159.9 MB (49.83%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,27 +1,18 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
         int[] ans = new int[nums.length];
-        int lessCount = 0;
-        int equalCount = 0;
+        int index = 0;
 
-        for(int num : nums) {
-            if(num < pivot) lessCount++;
-            else if(num == pivot) equalCount++;
+        for (int num : nums) {
+            if (num < pivot) ans[index++] = num;
         }
 
-        // Reserve the positions in array for less, equal and greater
-        int lessIndex = 0;
-        int equalIndex = lessCount;
-        int greaterIndex = lessCount + equalCount;
+        for (int num : nums) {
+            if (num == pivot) ans[index++] = num;
+        }
 
-        for(int num : nums) {
-            if(num < pivot) {
-                ans[lessIndex++] = num;
-            } else if(num == pivot) {
-                ans[equalIndex++] = num;
-            } else {
-                ans[greaterIndex++] = num;
-            }
+        for (int num : nums) {
+            if (num > pivot) ans[index++] = num;
         }
 
         return ans;
@@ -30,19 +21,28 @@ class Solution {
 
 /*
     int[] ans = new int[nums.length];
-        int index = 0;
+    int lessCount = 0;
+    int equalCount = 0;
 
-        for(int num : nums) {
-            if(num < pivot) ans[index++] = num;
+    for(int num : nums) {
+        if(num < pivot) lessCount++;
+        else if(num == pivot) equalCount++;
+    }
+
+    // Reserve the positions in array for less, equal and greater
+    int lessIndex = 0;
+    int equalIndex = lessCount;
+    int greaterIndex = lessCount + equalCount;
+
+    for(int num : nums) {
+        if(num < pivot) {
+            ans[lessIndex++] = num;
+        } else if(num == pivot) {
+            ans[equalIndex++] = num;
+        } else {
+            ans[greaterIndex++] = num;
         }
+    }
 
-        for(int num : nums) {
-            if(num == pivot) ans[index++] = num;
-        }
-
-        for(int num : nums) {
-            if(num > pivot) ans[index++] = num;
-        }
-
-        return ans;
+    return ans;
 */
