ef109a6948afc862ff5156a3a9e5bfe192fb56be
Rohan Figredo <rohanfigredo@gmail.com>
2026-06-08 19:36:03 +0530

    Time: 368 ms (58.17%), Space: 42.1 MB (62.34%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,15 +1,12 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        if (num % 2 != 0)
-            return false;
+        if (num % 2 != 0) return false;
         int end = num / 2;
         int sum = end;
 
         for (int i = 1; i < end; i++) {
-            if (num % i == 0)
-                sum += i;
-            if (sum > num)
-                return false;
+            if (num % i == 0) sum += i;
+            if (sum > num) return false;
         }
 
         if (sum == num) return true;
