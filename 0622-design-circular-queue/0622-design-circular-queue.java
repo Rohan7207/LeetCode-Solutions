166f6ba12bebc7b8814af352ad1c4e34e73611be
Rohan Figredo <rohanfigredo@gmail.com>
2026-06-12 20:02:29 +0530

    Time: 5 ms (32.17%), Space: 46.4 MB (84.66%) - LeetHub

--- 0622-design-circular-queue/0622-design-circular-queue.java
+++ 0622-design-circular-queue/0622-design-circular-queue.java
@@ -3,44 +3,50 @@ class MyCircularQueue {
     int cap;
     int head;
     int size;
+
     public MyCircularQueue(int k) {
-        arr=new int[k];
-        cap=k;
-        head=0;
-        size=0;
+        arr = new int[k];
+        cap = k;
+        head = 0;
+        size = 0;
     }
-    
+
     public boolean enQueue(int value) {
-        if(isFull()) return false;
-        int idx=(head+size)%cap;
-        arr[idx]=value;
+        if (isFull()) return false;
+
+        int idx = (head + size) % cap;
+        arr[idx] = value;
         size++;
+
         return true;
     }
-    
+
     public boolean deQueue() {
-        if(isEmpty()) return false;
-        head=(head+1)%cap;
+        if (isEmpty()) return false;
+
+        head = (head + 1) % cap;
         size--;
+
         return true;
     }
-    
+
     public int Front() {
-        return isEmpty()?-1:arr[head];
+        return isEmpty() ? -1 : arr[head];
     }
-    
+
     public int Rear() {
-        if(isEmpty()) return -1;
-        int idx=(head+size-1)%cap;
+        if (isEmpty()) return -1;
+        
+        int idx = (head + size - 1) % cap;
         return arr[idx];
     }
-    
+
     public boolean isEmpty() {
-        return size==0;
+        return size == 0;
     }
-    
+
     public boolean isFull() {
-        return size==cap;
+        return size == cap;
     }
 }
 
