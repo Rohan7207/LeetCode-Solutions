63f47a46ce41ffa12a3e5fb9157cfbafe5f97c39
Rohan Figredo <rohanfigredo@gmail.com>
2026-06-14 18:55:04 +0530

    Time: 1 ms (100%), Space: 46.7 MB (72.14%) - LeetHub

--- 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.java
+++ 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.java
@@ -3,14 +3,15 @@ class Solution {
         int res = 1;
         int max = 1;
 
-        for(int i = 1; i < nums.length; i++) {
-            if(nums[i] > nums[i - 1]) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] < nums[i + 1]) {
                 res++;
+                if (max < res) {
+                    max = res;
+                }
             } else {
                 res = 1;
             }
-
-            max = Math.max(max, res);
         }
 
         return max;
