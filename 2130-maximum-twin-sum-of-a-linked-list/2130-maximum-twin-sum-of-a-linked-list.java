e9dfeb0509d176147af733340d97248f24fd6469
Rohan Figredo <rohanfigredo@gmail.com>
2026-06-14 16:22:27 +0530

    Clean up code and add problem description
    Removed commented-out code and added problem description.

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -1,3 +1,36 @@
+// Problem: Maximum Twin Sum of a Linked List
+// Link: https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/?envType=daily-question&envId=2026-06-14
+// Difficulty: Medium
+
+// Approach:
+// We need to find the maximum twin sum
+// in an even-length linked list.
+// Twin nodes are:
+//     1st ↔ Last
+//     2nd ↔ Second Last
+//     3rd ↔ Third Last
+// Instead of storing values in an array,
+// use the linked list itself.
+// Step 1:
+//     Find the middle of the linked list
+//     using Slow and Fast pointers.
+// Step 2:
+//     Reverse the second half of the list.
+// Step 3:
+//     Use two pointers:
+//         p1 -> first half
+//         p2 -> reversed second half
+//     Compute:
+//         p1.val + p2.val
+//     for every twin pair.
+// Step 4:
+//     Keep track of the maximum twin sum.
+// Return the maximum sum found.
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -48,50 +81,3 @@ class Solution {
         return max;
     }
 }
-
-/*
-    public int pairSum(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        ListNode prev = null;
-
-        while (fast != null && fast.next != null) {
-            prev = slow;
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        if (prev != null)
-            prev.next = null;
-
-        ListNode reverseHead = reverseList(slow);
-
-        int max = 0;
-        while (head != null && reverseHead != null) {
-            int sum = 0;
-
-            sum = head.val + reverseHead.val;
-
-            max = Math.max(max, sum);
-
-            head = head.next;
-            reverseHead = reverseHead.next;
-        }
-
-        return max;
-    }
-
-    private ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode curr = head;
-
-        while (curr != null) {
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-
-        return prev;
-    }
-*/
