d200830fcfd1104427efedd22495541997530502
Rohan Figredo <rohanfigredo@gmail.com>
2026-06-15 20:27:51 +0530

    Update 0704-binary-search.java

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,3 +1,36 @@
+// Problem: Binary Search 
+// Link: https://leetcode.com/problems/binary-search/
+// Difficulty: Easy
+
+// Approach:
+// We need to find the target element
+// in a sorted array.
+// Use Binary Search.
+// Maintain:
+//     low  -> start of search space
+//     high -> end of search space
+// While search space exists:
+//     Find middle element.
+// If:
+//     nums[mid] == target
+//     Target found.
+//     Return mid.
+// Else if:
+//     target < nums[mid]
+//     Target must be in left half.
+//     high = mid - 1
+// Else:
+//     Target must be in right half.
+//     low = mid + 1
+// Continue until:
+//     low > high
+// If target is not found:
+//     return -1
+
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
+
+
 class Solution {
     public int search(int[] nums, int target) {
         int low = 0;
