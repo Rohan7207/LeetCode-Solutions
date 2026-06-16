296cd12247e04d1648af3c80b6ae7a86c80180b9
Rohan Figredo <rohanfigredo@gmail.com>
2026-06-16 20:14:15 +0530

    Time: 14 ms (99.98%), Space: 48.2 MB (88.25%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -1,20 +1,87 @@
+class ListNode {
+    int key, value;
+    ListNode next;
+
+    ListNode(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
 class MyHashMap {
-    int[] map = new int[1000001];
+    ListNode[] arr;
+    int size;
 
     public MyHashMap() {
-        Arrays.fill(map, -1);
+        size = 1007;
+        arr = new ListNode[size];
     }
-    
+
+    public int hash(int key) {
+        return key % size;
+    }
+
     public void put(int key, int value) {
-        map[key] = value;
+        int hashKey = hash(key);
+
+        if (arr[hashKey] == null) {
+            ListNode node = new ListNode(key, value);
+            arr[hashKey] = node;
+            return;
+        }
+
+        ListNode head = arr[hashKey];
+        ListNode curr = head;
+
+        while (curr != null) {
+            if (curr.key == key) {
+                curr.value = value;
+                return;
+            }
+
+            if (curr.next == null) break;
+
+            curr = curr.next;
+        }
+
+        ListNode node = new ListNode(key, value);
+        curr.next = node;
     }
-    
+
     public int get(int key) {
-        return map[key];
+        int hashKey = hash(key);
+        ListNode head = arr[hashKey];
+
+        if (head == null) return -1;
+
+        while (head != null) {
+            if (head.key == key) return head.value;
+
+            head = head.next;
+        }
+
+        return -1;
     }
-    
+
     public void remove(int key) {
-        map[key] = -1;
+        int hashKey = hash(key);
+        ListNode head = arr[hashKey];
+
+        if (head == null) return;
+
+        if (head.key == key) {
+            arr[hashKey] = head.next;
+            return;
+        }
+
+        while (head.next != null) {
+            if (head.next.key == key) {
+                head.next = head.next.next;
+                return;
+            }
+
+            head = head.next;
+        }
     }
 }
 
