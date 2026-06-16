0a472f03a956569806e9680ad3f61e9ac54ead85
Rohan Figredo <rohanfigredo@gmail.com>
2026-06-16 19:16:27 +0530

    Time: 4 ms (76.47%), Space: 55.3 MB (48.53%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -5,14 +5,14 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
 
-            if (ch == '*') {
-                if (str.length() > 0) {
-                    str.deleteCharAt(str.length() - 1);
-                }
-            } else if (ch == '#') {
+            if (ch == '#') {
                 str.append(str.toString());
             } else if (ch == '%') {
                 str.reverse();
+            } else if (ch == '*') {
+                if (str.length() > 0) {
+                    str.deleteCharAt(str.length() - 1);
+                }
             } else {
                 str.append(ch);
             }
