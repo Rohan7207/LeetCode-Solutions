33909fa3da3dd464f5d1e47b5d20cc2581384170
Rohan Figredo <rohanfigredo@gmail.com>
2026-06-17 18:41:17 +0530

    Time: 29 ms (73.08%), Space: 48.1 MB (15.38%) - LeetHub

--- 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.java
+++ 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.java
@@ -2,37 +2,38 @@ class Solution {
     public char processStr(String s, long k) {
         long len = 0;
 
-        for(char ch : s.toCharArray()) {
-            if(ch == '#') {
+        for (char ch : s.toCharArray()) {
+            if (ch == '#') {
                 len *= 2;
-            } else if(ch == '*') {
-                if(len > 0) len--;
-            } else if(ch == '%') {
+            } else if (ch == '*') {
+                if (len > 0) len--;
+            } else if (ch == '%') {
 
             } else {
                 len++;
             }
         }
 
-        if(k >= len) return '.';
+        if (k >= len)
+            return '.';
 
-        for(int i = s.length() - 1; i >= 0; i--) {
+        for (int i = s.length() - 1; i >= 0; i--) {
             char ch = s.charAt(i);
 
-            if(ch == '#') {
+            if (ch == '#') {
                 long oldLen = len / 2;
-                if(oldLen == 0) {
+                if (oldLen == 0) {
                     len = 0;
                     continue;
                 }
                 k = k % oldLen;
                 len = oldLen;
-            } else if(ch == '*') {
+            } else if (ch == '*') {
                 len++;
-            } else if(ch == '%') {
+            } else if (ch == '%') {
                 k = len - 1 - k;
             } else {
-                if(k == len - 1) {
+                if (k == len - 1) {
                     return ch;
                 }
 
