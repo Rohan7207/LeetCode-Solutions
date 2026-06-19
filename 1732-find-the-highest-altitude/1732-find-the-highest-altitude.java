99b8ee53c3a2a9c31190cdb5295b008c23523aac
Rohan Figredo <rohanfigredo@gmail.com>
2026-06-19 13:17:17 +0530

    Add method to calculate highest altitude
    Implemented a method to find the highest altitude from a given array of altitude changes.

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -1,3 +1,29 @@
+// Problem: Find the Highest Altitude
+// Link: https://leetcode.com/problems/find-the-highest-altitude/?envType=daily-question&envId=2026-06-19
+// Difficulty: Easy
+
+// Approach:
+// We are given an array gain[].
+// gain[i] represents the altitude change
+// between two points.
+// We start from altitude 0.
+// Maintain:
+//     altitude    -> current altitude
+//     maxAltitude -> highest altitude seen so far
+// Initially:
+//     altitude = 0
+//     maxAltitude = 0
+// Traverse gain array:
+//     Add gain[i] to altitude.
+//     altitude += gain[i]
+//     Now update maximum altitude:
+//     maxAltitude = max(maxAltitude, altitude)
+// Finally return maxAltitude.
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+
 class Solution {
     public int largestAltitude(int[] gain) {
         int altitude = 0;
@@ -7,6 +33,7 @@ class Solution {
             altitude += gain[i];
             maxAltitude = Math.max(maxAltitude, altitude);
         }
+        
         return maxAltitude;
     }
 }
