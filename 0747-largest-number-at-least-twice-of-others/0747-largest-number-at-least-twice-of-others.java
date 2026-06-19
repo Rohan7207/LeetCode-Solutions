e63fa3ac179b9bc73884748acc2b30fb71e55c59
Rohan Figredo <rohanfigredo@gmail.com>
2026-06-19 13:05:07 +0530

    Optimize dominantIndex method for finding largest number
    Refactored the dominantIndex method to improve efficiency by tracking the largest and second largest numbers in a single pass.

--- 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.java
+++ 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.java
@@ -1,3 +1,36 @@
+// Problem: Largest Number At Least Twice of Others
+// Link: https://leetcode.com/problems/largest-number-at-least-twice-of-others/
+// Difficulty: Easy
+
+// Approach:
+// We need to check whether the largest number
+// is at least twice the second largest number.
+// Why only second largest?
+//     If largest >= 2 * secondLargest,
+//     then largest is automatically at least twice
+//     every smaller number also.
+// So instead of checking largest with every element,
+// find:
+//     max       -> largest number
+//     secondMax -> second largest number
+//     index     -> index of largest number
+// Traverse the array:
+//     If nums[i] > max:
+//         Current number becomes new largest.
+//         Old max becomes secondMax.
+//         Update index.
+//     Else if nums[i] > secondMax:
+//         Current number becomes secondMax.
+// After traversal:
+//     If max >= 2 * secondMax:
+//         return index
+//     Else:
+//         return -1
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+
 class Solution {
     public int dominantIndex(int[] nums) {
         int max = -1;
@@ -21,23 +54,3 @@ class Solution {
         return -1;
     }
 }
-
-/*
-    int max = nums[0];
-    int maxIndex = 0;
-
-    for (int i = 1; i < nums.length; i++) {
-        if (max < nums[i]) {
-            max = nums[i];
-            maxIndex = i;
-        }
-    }
-
-    for (int i = 0; i < nums.length; i++) {
-        if (nums[i] != max && max < 2 * nums[i]) {
-            return -1;
-        }
-    }
-
-    return maxIndex;
-*/
