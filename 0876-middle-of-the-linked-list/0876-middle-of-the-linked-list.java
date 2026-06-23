ba4e73db9fc740c9f85926802a102ee884a6be8f
Rohan Figredo <rohanfigredo@gmail.com>
2026-06-23 19:30:09 +0530

    Add problem description and approach for linked list
    Added problem statement and approach explanation for finding the middle of a linked list.

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -1,3 +1,31 @@
+// Problem: Middle of the Linked List
+// Link: https://leetcode.com/problems/middle-of-the-linked-list/
+// Difficulty: Easy
+
+// Approach:
+// We need to find the middle node of the linked list.
+// Use two pointers:
+//     slow moves one step at a time
+//     fast moves two steps at a time
+// Since fast moves twice as fast as slow,
+// when fast reaches the end of the list,
+// slow will be at the middle.
+// Start both slow and fast at head.
+// Keep moving:
+//     slow = slow.next
+//     fast = fast.next.next
+// Continue while fast and fast.next are not null.
+// When the loop ends, slow is pointing to the middle node.
+// If the linked list has odd length:
+//     slow points to the exact middle.
+// If the linked list has even length:
+//     slow points to the second middle node.
+// Return slow.
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
