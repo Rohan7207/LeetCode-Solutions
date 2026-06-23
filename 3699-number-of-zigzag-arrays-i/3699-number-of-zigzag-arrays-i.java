728f044fea1997b0dfcaff3c2a6dfe8898d9a6a5
Rohan Figredo <rohanfigredo@gmail.com>
2026-06-23 18:56:11 +0530

    Refactor zigZagArrays method for better performance
    Refactor zigZagArrays method to improve clarity and efficiency. Added prefix and suffix sums for better performance in calculating newUp and newDown arrays.

--- 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.java
+++ 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.java
@@ -1,3 +1,75 @@
+// Problem: Number of ZigZag Arrays I
+// Link: https://leetcode.com/problems/number-of-zigzag-arrays-i/?envType=daily-question&envId=2026-06-23
+// Difficulty: Hard
+
+// Approach:
+// We need to count arrays of length n where:
+//     1. Every value is between l and r
+//     2. Adjacent values are not equal
+//     3. Directions must alternate
+// The third condition means we cannot have:
+//     a < b < c   // strictly increasing
+//     a > b > c   // strictly decreasing
+// So if the last move was UP, the next move must be DOWN.
+// If the last move was DOWN, the next move must be UP.
+// We use DP based on:
+//     last value
+//     last direction
+// Let:
+//     up[i] = number of valid arrays ending at value index i
+//             where the last move was UP
+//     down[i] = number of valid arrays ending at value index i
+//               where the last move was DOWN
+// Here value index i represents actual value:
+//     l + i
+// So total number of possible values is:
+//     m = r - l + 1
+// If n == 1:
+//     Every single value from l to r is valid.
+//     So answer is m.
+// For length 2:
+//     Any two different values are valid.
+// If previous value is smaller than current value,
+// then last move is UP.
+// So:
+//     up[i] = number of values smaller than i
+//           = i
+// If previous value is greater than current value,
+// then last move is DOWN.
+// So:
+//     down[i] = number of values greater than i
+//             = m - 1 - i
+// Now build arrays from length 3 to n.
+// To create newUp[i]:
+//     The current value is i.
+//     The previous value must be smaller than i.
+//     Also, previous move must be DOWN.
+// So:
+//     newUp[i] = sum of down[j] for all j < i
+// To create newDown[i]:
+//     The current value is i.
+//     The previous value must be greater than i.
+//     Also, previous move must be UP.
+// So:
+//     newDown[i] = sum of up[j] for all j > i
+// To calculate these sums efficiently:
+//     use prefix sum for down[]
+//     use suffix sum for up[]
+// Prefix helps newUp because smaller values are on the left.
+// Suffix helps newDown because greater values are on the right.
+// After building length n,
+// answer is:
+//     sum(up) + sum(down)
+// because a valid ZigZag array can end with either UP or DOWN.
+// Return answer modulo 1e9 + 7.
+
+// Time Complexity: O(n * m)
+// Space Complexity: O(m)
+//
+// where:
+//     m = r - l + 1
+
+
 class Solution {
     public int zigZagArrays(int n, int l, int r) {
         int MOD = 1000000007;
@@ -7,35 +79,24 @@ class Solution {
         if (n == 1)
             return m;
 
-        // up[i] = number of arrays ending at value index i where last move was UP
         long[] up = new long[m];
-        // down[i] = number of arrays ending at value index i where last move was DOWN
         long[] down = new long[m];
 
-        // initialize for length 2
         for (int i = 0; i < m; i++) {
             up[i] = i;
             down[i] = m - 1 - i;
         }
 
-        // up = {0, 1, 2} down = {2, 1, 0}
-        // build length 3 to n
         for (int len = 3; len <= n; len++) {
             long[] newUp = new long[m];
             long[] newDown = new long[m];
 
-            // prefix of down 
-            /*To create a new up ending at curr: previous value must be smaller than curr, previous direction must be down
-            So: newUp[curr] = sum of down[prev] where prev < curr*/
             long prefix = 0;
             for (int i = 0; i < m; i++) {
                 newUp[i] = prefix;
                 prefix = (prefix + down[i]) % MOD;
             }
 
-            // suffix of up
-            /*To create a new down ending at curr: previous value must be greater than curr previous direction must be up
-            So: newDown[curr] = sum of up[prev] where prev > curr */
             long suffix = 0;
             for (int i = m - 1; i >= 0; i--) {
                 newDown[i] = suffix;
