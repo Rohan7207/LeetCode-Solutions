324989e938762fe9dc25e1f0f4cbc00ec76ee969
Rohan Figredo <rohanfigredo@gmail.com>
2026-06-24 19:30:53 +0530

    Add problem description and approach comments
    Added problem description and approach for range sum of BST.

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.java
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.java
@@ -1,3 +1,32 @@
+// Problem: Range Sum of BST
+// Link: https://leetcode.com/problems/range-sum-of-bst/
+// Difficulty: Easy
+
+// Approach:
+// We need to find the sum of all node values in the BST
+// that lie between low and high.
+// Since it is a Binary Search Tree:
+//     left subtree values  < root.val
+//     right subtree values > root.val
+// We can use this property to avoid visiting unnecessary nodes.
+// If root is null:
+//     return 0
+// If root.val is between low and high:
+//     add root.val
+//     search both left and right subtree
+// If root.val is greater than or equal to low:
+//     possible valid values may exist in the left subtree
+//     so search left
+// If root.val is less than low:
+//     left subtree will also be smaller,
+//     so skip left and search right
+// Finally return the total sum.
+
+// Time Complexity: O(n) in worst case
+// Space Complexity: O(h)
+//     h = height of tree due to recursion stack
+
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
