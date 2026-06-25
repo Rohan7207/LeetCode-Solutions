b91f5e7e95bca1d3b6bec843d8e15970b0b6a4eb
Rohan Figredo <rohanfigredo@gmail.com>
2026-06-25 19:06:52 +0530

    Time: 12 ms (90.15%), Space: 47.6 MB (8.92%) - LeetHub

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.java
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.java
@@ -1,23 +1,84 @@
 class Solution {
     public int countMajoritySubarrays(int[] nums, int target) {
-        int ans = 0;
+        int n = nums.length;
 
-        for (int i = 0; i < nums.length; i++) {
-            int count = 0;
+        // Step 1: Convert array into +1 and -1
+        int[] prefix = new int[n + 1];
 
-            for (int j = i; j < nums.length; j++) {
-                if (nums[j] == target) {
-                    count++;
-                }
+        for (int i = 0; i < n; i++) {
 
-                int l = j - i + 1;
-
-                if (count > l / 2) {
-                    ans++;
-                }
+            if (nums[i] == target) {
+                prefix[i + 1] = prefix[i] + 1;
+            } else {
+                prefix[i + 1] = prefix[i] - 1;
             }
         }
 
+        // Step 2: Coordinate compression
+        int[] sorted = prefix.clone();
+
+        Arrays.sort(sorted);
+
+        List<Integer> unique = new ArrayList<>();
+
+        for (int x : sorted) {
+
+            if (unique.size() == 0 ||
+                unique.get(unique.size() - 1) != x) {
+
+                unique.add(x);
+            }
+        }
+
+        // value -> rank mapping
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < unique.size(); i++) {
+
+            map.put(unique.get(i), i + 1);
+        }
+
+        // Step 3: Fenwick Tree
+        int[] fenwick = new int[unique.size() + 1];
+        int ans = 0;
+
+        // Add empty prefix sum = 0
+        add(fenwick, map.get(0), 1);
+
+        // Step 4: Process prefix sums
+        for (int i = 1; i <= n; i++) {
+            int current = prefix[i];
+            int rank = map.get(current);
+
+            // count previous prefix sums smaller than current
+            ans += query(fenwick, rank - 1);
+
+            // store current prefix
+            add(fenwick, rank, 1);
+        }
+
         return ans;
+    }
+
+    // Fenwick update
+    private void add(int[] fenwick, int index, int value) {
+        while (index < fenwick.length) {
+            fenwick[index] += value;
+
+            index += index & -index;
+        }
+    }
+
+    // Fenwick prefix sum query
+    private int query(int[] fenwick, int index) {
+        int sum = 0;
+
+        while (index > 0) {
+            sum += fenwick[index];
+
+            index -= index & -index;
+        }
+
+        return sum;
     }
 }
