c991e7d44ce8b63f0edfe88285fcd46e2a23f4d1
Rohan Figredo <rohanfigredo@gmail.com>
2026-06-27 20:52:50 +0530

    Time: 2 ms (95.19%), Space: 47.7 MB (50.54%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,22 +1,42 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        Map<Integer, Integer> freq = new HashMap<>();
+        int[] ans = new int[2];
+        int[] count = new int[nums.length + 1];
 
         for (int num : nums) {
-            freq.put(num, freq.getOrDefault(num, 0) + 1);
+            count[num]++;
         }
 
-        int[] ans = new int[2];
         for (int i = 1; i <= nums.length; i++) {
-            if (freq.containsKey(i)) {
-                if (freq.get(i) == 2) {
-                    ans[0] = i;
-                }
-            } else {
+            if (count[i] == 2)
+                ans[0] = i;
+
+            if (count[i] == 0)
                 ans[1] = i;
-            }
         }
 
         return ans;
     }
 }
+
+/*
+    // Solution with hashmap
+    Map<Integer, Integer> freq = new HashMap<>();
+
+    for (int num : nums) {
+        freq.put(num, freq.getOrDefault(num, 0) + 1);
+    }
+
+    int[] ans = new int[2];
+    for (int i = 1; i <= nums.length; i++) {
+        if (freq.containsKey(i)) {
+            if (freq.get(i) == 2) {
+                ans[0] = i;
+            }
+        } else {
+            ans[1] = i;
+        }
+    }
+
+    return ans;
+*/
