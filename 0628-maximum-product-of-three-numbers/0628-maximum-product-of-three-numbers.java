05b388db7e7e8add012363b91bb938264eddc6f1
Rohan Figredo <rohanfigredo@gmail.com>
2026-06-29 19:02:36 +0530

    Time: 24 ms (5.71%), Space: 47.1 MB (96.92%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -1,5 +1,41 @@
 class Solution {
     public int maximumProduct(int[] nums) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(a, b));
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
+
+        for (int num : nums) {
+            // Track 3 largest values
+            minHeap.add(num);
+
+            if (minHeap.size() > 3) {
+                minHeap.poll();
+            }
+
+            // Track 2 smallest values
+            maxHeap.add(num);
+
+            if (maxHeap.size() > 2) {
+                maxHeap.poll();
+            }
+        }
+
+        int max1 = minHeap.poll();
+        int max2 = minHeap.poll();
+        int max3 = minHeap.poll();
+
+        int min1 = maxHeap.poll();
+        int min2 = maxHeap.poll();
+
+        int opt1 = max1 * max2 * max3;
+        int opt2 = min1 * min2 * max3;
+
+        return Math.max(opt1, opt2);
+    }
+}
+
+/*
+    Using sorting O(nlogn) above O(n) which uses minheap to store 3 max values and maxheap to store
+    2 min values and multiply 2 min with 3 max value
         Arrays.sort(nums);
         int n = nums.length;
 
@@ -8,26 +44,4 @@ class Solution {
         int opt2 = nums[0] * nums[1] * nums[n - 1];
 
         return Math.max(opt1, opt2);
-    }
-}
-
-/*
-    PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(a, b));
-
-        for (int num : nums) {
-            minHeap.add(num);
-
-            if (minHeap.size() > 3) {
-                minHeap.poll();
-            }
-        }
-
-        int ans = 1;
-        while (!minHeap.isEmpty()) {
-            int curr = minHeap.poll();
-
-            ans *= curr;
-        }
-
-        return ans;
 */
