27382cb952023669c22656592ba697ee567b6dbe
Rohan Figredo <rohanfigredo@gmail.com>
2026-07-02 16:48:17 +0530

    Time: 30 ms (38.93%), Space: 46.6 MB (97.54%) - LeetHub

---
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.java
@@ -0,0 +1,48 @@
+class Solution {
+    int[][] directions = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+
+    public boolean findSafeWalk(List<List<Integer>> grid, int health) {
+        int m = grid.size();
+        int n = grid.get(0).size();
+
+        int[][] bestHealth = new int[m][n];
+        Queue<int[]> q = new LinkedList<>();
+        if (grid.get(0).get(0) == 1) {
+            health--;
+        }
+
+        if (health <= 0)
+            return false;
+
+        q.offer(new int[] { 0, 0, health });
+        bestHealth[0][0] = health;
+
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
+
+            int r = curr[0];
+            int c = curr[1];
+            int currHealth = curr[2];
+
+            if (r == m - 1 && c == n - 1 && currHealth > 0) {
+                return true;
+            }
+
+            for (int[] dir : directions) {
+                int nr = r + dir[0];
+                int nc = c + dir[1];
+
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n) {
+                    int newHealth = currHealth - grid.get(nr).get(nc);
+
+                    if (newHealth > 0 && newHealth > bestHealth[nr][nc]) {
+                        bestHealth[nr][nc] = newHealth;
+                        q.offer(new int[] { nr, nc, newHealth });
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+}
