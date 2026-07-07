b1e72623ffc913faba5fbfd4ce999c8892f8afcd
Rohan Figredo <rohanfigredo@gmail.com>
2026-07-07 19:20:23 +0530

    Time: 15 ms (99.31%), Space: 98.2 MB (60.05%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        float[] time = new float[target + 1];
+        for (int i = 0; i < speed.length; i++) {
+            time[position[i]] = (float) (target - position[i]) / speed[i];
+        }
+
+        int count = 0;
+        float prevtime = 0;
+        for (int i = time.length - 1; i >= 0; i--) {
+            if (time[i] > prevtime) {
+                count++;
+                prevtime = time[i];
+            }
+        }
+        
+        return count;
+    }
+}
