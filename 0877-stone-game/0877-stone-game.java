fc8d198f114dc8564e5d2ff53121692ada749f4b
Rohan Figredo <rohanfigredo@gmail.com>
2026-07-12 19:12:13 +0530

    Time: 0 ms (100%), Space: 42.9 MB (76.43%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,28 +1,29 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
+        return true;
         // Maximum score difference the current player can achieve from piles[left...right].
-        int n = piles.length;
-        int[][] dp = new int[n][n];
+        // int n = piles.length;
+        // int[][] dp = new int[n][n];
 
-        // Fill the base case left == right
-        for (int i = 0; i < n; i++) {
-            dp[i][i] = piles[i];
-        }
+        // // Fill the base case left == right
+        // for (int i = 0; i < n; i++) {
+        //     dp[i][i] = piles[i];
+        // }
 
-        // Decide traversal order, Start with intervals of length 2.
-        for (int len = 2; len <= n; len++) {
-            // Find left and right
-            for (int left = 0; left <= n - len; left++) {
-                int right = left + len - 1;
+        // // Decide traversal order, Start with intervals of length 2.
+        // for (int len = 2; len <= n; len++) {
+        //     // Find left and right
+        //     for (int left = 0; left <= n - len; left++) {
+        //         int right = left + len - 1;
 
-                // Fill recurrence
-                int takeLeft = piles[left] - dp[left + 1][right];
-                int takeRight = piles[right] - dp[left][right - 1];
+        //         // Fill recurrence
+        //         int takeLeft = piles[left] - dp[left + 1][right];
+        //         int takeRight = piles[right] - dp[left][right - 1];
 
-                dp[left][right] = Math.max(takeLeft, takeRight);
-            }
-        }
+        //         dp[left][right] = Math.max(takeLeft, takeRight);
+        //     }
+        // }
 
-        return dp[0][n - 1] > 0;
+        // return dp[0][n - 1] > 0;
     }
 }
