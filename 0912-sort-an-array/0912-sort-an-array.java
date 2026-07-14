e9c6ecaa14b56827dfcd57988d9de8d0e5e43a61
Rohan Figredo <rohanfigredo@gmail.com>
2026-07-14 19:20:49 +0530

    Time: 36 ms (24.68%), Space: 78.1 MB (90.63%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,18 +1,50 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(a, b));
+        int n = nums.length;
+        return quickSort(nums, 0, n - 1);
+    }
 
-        for(int num : nums) {
+    private int[] quickSort(int[] nums, int start, int end) {
+        int pivot = nums[start + new Random().nextInt(end - start + 1)];
+
+        int i = start, j = end;
+
+        while (i <= j) {
+            while (nums[i] < pivot)
+                i++;
+            while (nums[j] > pivot)
+                j--;
+
+            if (i <= j) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        if (i < end)
+            quickSort(nums, i, end);
+        if (j > start)
+            quickSort(nums, start, j);
+        return nums;
+    }
+}
+
+/*
+     PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(a, b));
+
+        for (int num : nums) {
             minHeap.add(num);
         }
 
         int[] ans = new int[minHeap.size()];
         int index = 0;
 
-        while(!minHeap.isEmpty()) {
+        while (!minHeap.isEmpty()) {
             ans[index++] = minHeap.poll();
         }
 
         return ans;
-    }
-}
+*/
