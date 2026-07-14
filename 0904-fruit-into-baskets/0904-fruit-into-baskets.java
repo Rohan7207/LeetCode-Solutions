1b1e2d8bba60405b9ebad8e6d898f22da8ab0af5
Rohan Figredo <rohanfigredo@gmail.com>
2026-07-14 19:42:36 +0530

    Time: 47 ms (89.48%), Space: 66.1 MB (96.57%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        int ans = 0;
+
+        int left = 0;
+        for(int right = 0; right < fruits.length; right++) {
+            freqMap.put(fruits[right], freqMap.getOrDefault(fruits[right], 0) + 1);
+
+            while(freqMap.size() > 2) {
+                int currFreq = freqMap.get(fruits[left]);
+                currFreq--;
+
+                if(currFreq == 0) {
+                    freqMap.remove(fruits[left]);
+                } else {
+                    freqMap.put(fruits[left], currFreq);
+                }
+
+                left++;
+            }
+
+            ans = Math.max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+}
