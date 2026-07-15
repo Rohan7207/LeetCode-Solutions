3d775401839455fdb151b21cfd49fdcf96a1f1e5
Rohan Figredo <rohanfigredo@gmail.com>
2026-07-15 15:26:48 +0530

    Time: 11 ms (67.36%), Space: 47.5 MB (18.9%) - LeetHub

--- 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.java
+++ 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.java
@@ -3,12 +3,12 @@ class Solution {
         int n = deck.length;
         Map<Integer, Integer> freqMap = new HashMap<>();
 
-        for(int card : deck) {
+        for (int card : deck) {
             freqMap.put(card, freqMap.getOrDefault(card, 0) + 1);
         }
 
         int gcdValue = 0;
-        for(int freq : freqMap.values()) {
+        for (int freq : freqMap.values()) {
             gcdValue = gcd(gcdValue, freq);
         }
 
@@ -16,7 +16,7 @@ class Solution {
     }
 
     private int gcd(int a, int b) {
-        while(b > 0) {
+        while (b > 0) {
             int temp = a % b;
             a = b;
             b = temp;
