8167989fdcb7e7161cfe48a1947cbd43802564cd
Rohan Figredo <rohanfigredo@gmail.com>
2026-07-16 20:10:29 +0530

    Time: 15 ms (61.06%), Space: 51.7 MB (19.59%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -2,7 +2,6 @@ class Solution {
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
-
         int i = 0, j = 0;
 
         while (i < g.length && j < s.length) {
@@ -13,7 +12,7 @@ class Solution {
                 j++;
             }
         }
-        
+
         return i;
     }
 }
