50e0bae99a40dc82af56cf1ce8466cc110955cc2
Rohan Figredo <rohanfigredo@gmail.com>
2026-07-20 16:39:09 +0530

    Time: 5 ms (83.45%), Space: 46.7 MB (97.47%) - LeetHub

---
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -0,0 +1,47 @@
+class Solution {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        // Total number of elements
+        int total = m * n;
+
+        // Reduce unnecessary rotations
+        k %= total;
+
+        // Create a new grid to store shifted elements
+        int[][] ans = new int[m][n];
+
+        // Traverse every element
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                // Convert (i, j) to 1D index
+                int index = i * n + j;
+
+                // Find the new shifted index
+                int newIndex = (index + k) % total;
+
+                // Convert new 1D index back to (row, col)
+                int newRow = newIndex / n;
+                int newCol = newIndex % n;
+
+                // Place current element in its new position
+                ans[newRow][newCol] = grid[i][j];
+            }
+        }
+
+        // Convert int[][] to List<List<Integer>>
+        List<List<Integer>> list = new ArrayList<>();
+
+        for(int[] row : ans) {
+            List<Integer> innerList = new ArrayList<>();
+            for(int val : row) {
+                innerList.add(val);
+            }
+
+            list.add(innerList);
+        }
+
+        return list;
+    }
+}
