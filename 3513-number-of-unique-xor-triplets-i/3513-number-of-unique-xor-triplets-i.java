68ff49d7fbdc082c108801a6f5f228ad70a131a7
Rohan Figredo <rohanfigredo@gmail.com>
2026-07-23 18:29:43 +0530

    Time: 0 ms (100%), Space: 125.6 MB (10.2%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
@@ -2,7 +2,8 @@ class Solution {
     public int uniqueXorTriplets(int[] nums) {
         int n = nums.length;
 
-        if (n <= 2) return n;
+        if (n <= 2)
+            return n;
 
         int ans = 1;
         while (ans <= n) {
@@ -18,4 +19,8 @@ ans = 1   (1 <= 5) ✓
 ans = 2   (2 <= 5) ✓
 ans = 4   (4 <= 5) ✓
 ans = 8   (8 <= 5) ✗ Stop
+
+or 
+if (n <= 2) return n;
+return Integer.highestOneBit(n) << 1;
 */
