2d8f99ae518cccd6ac9cc898b22f015917aa2b80
Rohan Figredo <rohanfigredo@gmail.com>
2026-07-24 14:02:53 +0530

    Time: 166 ms (100%), Space: 46.8 MB (96.36%) - LeetHub

---
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+        boolean[] one = new boolean[2048];
+        boolean[] two = new boolean[2048];
+        boolean[] three = new boolean[2048];
+
+        for(int num : nums) {
+            one[num] = true;
+        }
+
+        for(int xor = 0; xor < 2048; xor++) {
+            if(!one[xor]) {
+                continue;
+            }
+
+            for(int num : nums) {
+                two[xor ^ num] = true;
+            }
+        }
+
+        for(int xor = 0; xor < 2048; xor++) {
+            if(!two[xor]) {
+                continue;
+            }
+
+            for(int num : nums) {
+                three[xor ^ num] = true;
+            }
+        }
+
+        int count = 0;
+        for(int xor = 0; xor < 2048; xor++) {
+            if(three[xor]) {
+                count++;
+            }
+        }
+
+
+        return count;
+    }
+}
