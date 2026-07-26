10ad60c37aeb30177f81956966e0a0b321999290
Rohan Figredo <rohanfigredo@gmail.com>
2026-07-26 14:11:41 +0530

    Time: 2 ms (99.61%), Space: 47.3 MB (89.75%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -1,6 +1,38 @@
 class Solution {
     public int maximumProduct(int[] nums) {
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(a, b));
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
+
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for(int num : nums) {
+            if(num >= max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            } else if(num >= max2) {
+                max3 = max2;
+                max2 = num;
+            } else if(num >= max3) {
+                max3 = num;
+            }
+
+            if(num <= min1) {
+                min2 = min1;
+                min1 = num;
+            } else if(num <= min2) {
+                min2 = num;
+            }
+        }
+
+        return Math.max(max1 * max2 * max3, min1 * min2 * max1);
+    }
+}
+
+/*
+     PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(a, b));
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
 
         for (int num : nums) {
@@ -30,8 +62,7 @@ class Solution {
         int opt2 = min1 * min2 * max3;
 
         return Math.max(opt1, opt2);
-    }
-}
+*/
 
 // Time Complexity: O(n log 3)
 // Space Complexity: O(1)
