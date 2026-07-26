b4324b0b54d28d8ab80a093d22c28a4ee7fed079
Rohan Figredo <rohanfigredo@gmail.com>
2026-07-26 14:24:01 +0530

    Time: 1 ms (92.37%), Space: 44.4 MB (94.91%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.java
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.java
@@ -1,43 +1,49 @@
-// Problem: Check if Array is Good
-// Link: https://leetcode.com/problems/check-if-array-is-good/?envType=daily-question&envId=2026-05-14
-// Difficulty: Easy
-
-// Approach:
-// Find the maximum value in the array.
-// If array length is not equal to max value + 1,
-// return false.
-// Use a frequency array to count occurrences
-// of every number.
-// Check whether every number from 1 to n-1
-// appears exactly once.
-// Finally check whether number n appears twice.
-// Return true if all conditions satisfy,
-// otherwise return false.
-
-// Time Complexity: O(n)
-// Space Complexity: O(n)
-
-
 class Solution {
     public boolean isGood(int[] nums) {
         int n = 0;
 
-        for(int num : nums) {
+        for (int num : nums) {
             n = Math.max(n, num);
         }
 
-        if(nums.length != n + 1) return false;
+        if (nums.length != n + 1) return false;
 
         int[] freq = new int[201];
 
-        for(int num : nums) {
+        for (int num : nums) {
             freq[num]++;
         }
 
-        for(int i = 1; i < n; i++) {
-            if(freq[i] != 1) return false;
+        // Every number from 1 to n-1 appears exactly once
+        for (int i = 1; i < n; i++) {
+            if (freq[i] != 1) return false;
         }
 
         return freq[n] == 2;
     }
 }
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+// “I use a frequency array to count occurrences and verify whether numbers follow the required repetition pattern.”
+
+/*
+    Set<Integer> seen = new HashSet<>();
+        int n = nums.length - 1;
+        boolean flag = false;
+
+        for(int num : nums) {
+            if(num > n) return false;
+
+            if(seen.contains(num)) {
+                if(num < n || flag) return false;
+                flag = true;
+                continue;
+            }
+
+            seen.add(num);
+        }
+
+        return true;
+*/
