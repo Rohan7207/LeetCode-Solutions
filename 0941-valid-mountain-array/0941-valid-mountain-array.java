5dcb4856a41b6506b646f37ae9631e95e675cc25
Rohan Figredo <rohanfigredo@gmail.com>
2026-07-27 15:10:33 +0530

    Time: 1 ms (100%), Space: 47.6 MB (33.89%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.java
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -4,23 +4,23 @@ class Solution {
         boolean flag2 = false;
         int n = arr.length;
 
-        if(n < 3) {
+        if (n < 3) {
             return false;
         }
 
         int i = 1;
 
-        while(i < n && arr[i] > arr[i - 1]) {
+        while (i < n && arr[i] > arr[i - 1]) {
             i++;
             flag1 = true;
         }
 
-        while(i < n && arr[i] < arr[i - 1]) {
+        while (i < n && arr[i] < arr[i - 1]) {
             i++;
             flag2 = true;
         }
 
-        if(i == n && flag1 && flag2) {
+        if (i == n && flag1 && flag2) {
             return true;
         }
 
