7c385f4b9fd4d3fb349723c3839773119a6198f2
Rohan Figredo <rohanfigredo@gmail.com>
2026-07-27 15:08:54 +0530

    Time: 1 ms (100%), Space: 47.6 MB (33.89%) - LeetHub

---
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        boolean flag1 = false;
+        boolean flag2 = false;
+        int n = arr.length;
+
+        if(n < 3) {
+            return false;
+        }
+
+        int i = 1;
+
+        while(i < n && arr[i] > arr[i - 1]) {
+            i++;
+            flag1 = true;
+        }
+
+        while(i < n && arr[i] < arr[i - 1]) {
+            i++;
+            flag2 = true;
+        }
+
+        if(i == n && flag1 && flag2) {
+            return true;
+        }
+
+        return false;
+    }
+}
