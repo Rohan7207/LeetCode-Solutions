9d0b5f8ca817346836c5026b64453f999f78d98a
Rohan Figredo <rohanfigredo@gmail.com>
2026-07-28 15:01:45 +0530

    Time: 44 ms (41.7%), Space: 80.2 MB (22.44%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.java
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.java
@@ -6,8 +6,9 @@ class Solution {
         // 1 2 3 4 5 7
         for(int i = 1; i < nums.length; i++) {
             if(nums[i] <= nums[i - 1]) {
-                moves += (nums[i - 1] + 1) - nums[i];
-                nums[i] = nums[i - 1] + 1;
+                int need = nums[i - 1] + 1;
+                moves += need - nums[i];
+                nums[i] = need;
             }
         }
 
