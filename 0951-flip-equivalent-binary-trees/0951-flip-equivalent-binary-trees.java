2ac2366fef1f591111f4bc6383c1fca98d8a5734
Rohan Figredo <rohanfigredo@gmail.com>
2026-07-29 20:11:34 +0530

    Time: 0 ms (100%), Space: 43.2 MB (58.02%) - LeetHub

--- 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.java
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.java
@@ -19,23 +19,23 @@ class Solution {
     }
 
     private boolean dfs(TreeNode root1, TreeNode root2) {
-        if(root1 == null && root2 == null) {
+        if (root1 == null && root2 == null) {
             return true;
         }
 
-        if(root1 == null || root2 == null) {
+        if (root1 == null || root2 == null) {
             return false;
         }
 
-        if(root1.val != root2.val) {
+        if (root1.val != root2.val) {
             return false;
         }
 
-        boolean noFlip = dfs(root1.left, root2.left) && 
-                            dfs(root1.right, root2.right);
+        boolean noFlip = dfs(root1.left, root2.left) &&
+                dfs(root1.right, root2.right);
 
-        boolean flip = dfs(root1.left, root2.right) && 
-                            dfs(root1.right, root2.left);
+        boolean flip = dfs(root1.left, root2.right) &&
+                dfs(root1.right, root2.left);
 
         return noFlip || flip;
     }
