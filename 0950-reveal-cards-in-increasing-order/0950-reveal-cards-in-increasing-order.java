a2f10560307e1a800117ed7ba5c47d50923b6e82
Rohan Figredo <rohanfigredo@gmail.com>
2026-07-29 19:56:07 +0530

    Time: 7 ms (93.17%), Space: 45.7 MB (75.68%) - LeetHub

---
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] deckRevealedIncreasing(int[] deck) {
+        Arrays.sort(deck);
+        int n = deck.length;
+
+        Deque<Integer> dq = new ArrayDeque<>();
+
+        for(int i = n - 1; i >= 0; i--) {
+            if(!dq.isEmpty()) {
+                dq.addFirst(dq.removeLast());
+            }
+
+            dq.addFirst(deck[i]);
+        }
+
+        int[] ans = new int[n];
+        int idx = 0;
+
+        for(int num : dq) {
+            ans[idx] = num;
+            idx++;
+        }
+
+        return ans;
+    }
+}
