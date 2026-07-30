4fa0ce4a9b30594e3b4ebd05690d118cf44bc71d
Rohan Figredo <rohanfigredo@gmail.com>
2026-07-30 15:29:26 +0530

    Time: 92 ms (22.04%), Space: 53.7 MB (53.88%) - LeetHub

--- 0954-array-of-doubled-pairs/0954-array-of-doubled-pairs.java
+++ 0954-array-of-doubled-pairs/0954-array-of-doubled-pairs.java
@@ -5,23 +5,23 @@ class Solution {
 
         Integer[] nums = new Integer[n];
 
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             nums[i] = arr[i];
         }
 
         Arrays.sort(nums, (a, b) -> Integer.compare(Math.abs(a), Math.abs(b)));
 
-        for(int num : nums) {
+        for (int num : nums) {
             freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
 
-        for(int x : nums) {
-            if(freqMap.get(x) == 0) {
+        for (int x : nums) {
+            if (freqMap.get(x) == 0) {
                 continue;
             }
 
             // Is there at least one unused double available
-            if(freqMap.getOrDefault(2 * x, 0) == 0) {
+            if (freqMap.getOrDefault(2 * x, 0) == 0) {
                 return false;
             }
 
