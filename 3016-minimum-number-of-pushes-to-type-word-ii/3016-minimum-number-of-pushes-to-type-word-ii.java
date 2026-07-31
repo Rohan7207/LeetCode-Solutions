68a1a660338acf2b339816a93ba71c6e1b7f2a9d
Rohan Figredo <rohanfigredo@gmail.com>
2026-07-31 15:29:05 +0530

    Time: 10 ms (66.76%), Space: 48 MB (52.6%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -4,23 +4,24 @@ class Solution {
         int[] freq = new int[26];
 
         // Count occurrences of each letter
-        for(char ch : word.toCharArray()) {
+        for (char ch : word.toCharArray()) {
             freq[ch - 'a']++;
         }
 
         // Sort the frequncies in descending order
         Arrays.sort(freq);
         int[] sortedFreq = new int[26];
-        for(int i = 0; i < 26; i++) {
+        for (int i = 0; i < 26; i++) {
             sortedFreq[i] = freq[25 - i];
         }
 
         int totalPushes = 0;
 
         // Calculate total no.of presses
-        for(int i = 0; i < 26; i++) {
-            if(sortedFreq[i] == 0) break;
-            totalPushes += ((i / 8) + 1 ) * sortedFreq[i];
+        for (int i = 0; i < 26; i++) {
+            if (sortedFreq[i] == 0)
+                break;
+            totalPushes += ((i / 8) + 1) * sortedFreq[i];
         }
 
         return totalPushes;
