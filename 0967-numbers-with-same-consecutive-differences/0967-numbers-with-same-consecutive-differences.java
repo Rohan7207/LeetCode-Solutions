3b7d03c326d5f796698ad4a558bed15e9ef84368
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-02 18:53:38 +0530

    Time: 1 ms (100%), Space: 43.3 MB (86.01%) - LeetHub

--- 0967-numbers-with-same-consecutive-differences/0967-numbers-with-same-consecutive-differences.java
+++ 0967-numbers-with-same-consecutive-differences/0967-numbers-with-same-consecutive-differences.java
@@ -2,13 +2,13 @@ class Solution {
     public int[] numsSameConsecDiff(int n, int k) {
         List<Integer> list = new ArrayList<>();
 
-        for(int digit = 1; digit <= 9; digit++) {
+        for (int digit = 1; digit <= 9; digit++) {
             dfs(list, digit, n - 1, k);
         }
 
         int size = list.size();
         int[] ans = new int[size];
-        for(int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) {
             ans[i] = list.get(i);
         }
 
@@ -16,25 +16,25 @@ class Solution {
     }
 
     private void dfs(List<Integer> list, int currNum, int remainingDigits, int k) {
-        if(remainingDigits == 0) {
+        if (remainingDigits == 0) {
             list.add(currNum);
             return;
         }
 
         int last = currNum % 10;
-        if(k == 0) {
+        if (k == 0) {
             int newNumber = currNum * 10 + last;
             dfs(list, newNumber, remainingDigits - 1, k);
         } else {
             int nextDigit1 = last - k;
             int nextDigit2 = last + k;
 
-            if(nextDigit1 >= 0 && nextDigit1 <= 9) {
+            if (nextDigit1 >= 0 && nextDigit1 <= 9) {
                 int newNumber = currNum * 10 + nextDigit1;
                 dfs(list, newNumber, remainingDigits - 1, k);
             }
 
-            if(nextDigit2 >= 0 && nextDigit2 <= 9) {
+            if (nextDigit2 >= 0 && nextDigit2 <= 9) {
                 int newNumber = currNum * 10 + nextDigit2;
                 dfs(list, newNumber, remainingDigits - 1, k);
             }
