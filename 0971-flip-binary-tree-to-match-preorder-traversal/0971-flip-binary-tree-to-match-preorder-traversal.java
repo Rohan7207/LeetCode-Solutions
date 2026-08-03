4293138b64166d01f3590c4ef297dd321cb1a66d
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-03 15:00:52 +0530

    Time: 0 ms (100%), Space: 43.9 MB (57.32%) - LeetHub

--- 0971-flip-binary-tree-to-match-preorder-traversal/0971-flip-binary-tree-to-match-preorder-traversal.java
+++ 0971-flip-binary-tree-to-match-preorder-traversal/0971-flip-binary-tree-to-match-preorder-traversal.java
@@ -22,26 +22,26 @@ class Solution {
     public List<Integer> flipMatchVoyage(TreeNode root, int[] voyage) {
         dfs(root, voyage);
 
-        if(!possible) {
+        if (!possible) {
             return Arrays.asList(-1);
         }
-        
+
         return ans;
     }
 
     private void dfs(TreeNode root, int[] voyage) {
-        if(root == null) {
+        if (root == null) {
             return;
         }
 
-        if(root.val != voyage[index]) {
+        if (root.val != voyage[index]) {
             possible = false;
             return;
         }
 
         index++;
 
-        if(root.left != null && index < voyage.length && root.left.val != voyage[index]) {
+        if (root.left != null && index < voyage.length && root.left.val != voyage[index]) {
             ans.add(root.val);
             dfs(root.right, voyage);
             dfs(root.left, voyage);
