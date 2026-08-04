02896798861ad84ece52be3f100ff7b4262c62fa
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-04 15:00:20 +0530

    Time: 11 ms (98.37%), Space: 47.7 MB (39.92%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.java
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.java
@@ -3,9 +3,9 @@ class Solution {
         //  The necessary and sufficient condition for these lengths a, b and c to form a triangle of non-zero area is a+b>c.
         Arrays.sort(nums);
 
-        for(int i = nums.length - 1; i >= 2; i--) {
+        for (int i = nums.length - 1; i >= 2; i--) {
             // Triangle inequality condition: sum of two smaller sides > largest side
-            if(nums[i - 2] + nums[i - 1] > nums[i]) {
+            if (nums[i - 2] + nums[i - 1] > nums[i]) {
                 return nums[i - 2] + nums[i - 1] + nums[i];
             }
         }
