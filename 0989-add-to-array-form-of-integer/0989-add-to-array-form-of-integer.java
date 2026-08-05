3346fe1d10a40f05108d7c76b86a3acf4a56302b
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-05 15:29:25 +0530

    Time: 3 ms (74.09%), Space: 47.7 MB (67.3%) - LeetHub

---
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        List<Integer> res = new ArrayList<>();
+        int i = num.length - 1;
+
+        // Loop as long as there are digits left in the array OR k has a value to carry over
+        while(i >= 0 || k > 0) {
+            if(i >= 0) {
+                k += num[i];  // Add the current array digit to k
+                i--;
+            }
+
+            res.add(k % 10);   // Add the last digit of the running sum to the result
+            k /= 10;           // Carry over the remaining value
+        }
+
+        // Since we added digits from right to left, the list is backwards. Reverse it.
+        Collections.reverse(res);
+        return res;
+    }
+}
