61c94fdcfa59c47aed4d9715609bdcba04778021
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-05 15:29:59 +0530

    Time: 2 ms (99.47%), Space: 47.7 MB (49.45%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
@@ -4,14 +4,14 @@ class Solution {
         int i = num.length - 1;
 
         // Loop as long as there are digits left in the array OR k has a value to carry over
-        while(i >= 0 || k > 0) {
-            if(i >= 0) {
-                k += num[i];  // Add the current array digit to k
+        while (i >= 0 || k > 0) {
+            if (i >= 0) {
+                k += num[i]; // Add the current array digit to k
                 i--;
             }
 
-            res.add(k % 10);   // Add the last digit of the running sum to the result
-            k /= 10;           // Carry over the remaining value
+            res.add(k % 10); // Add the last digit of the running sum to the result
+            k /= 10; // Carry over the remaining value
         }
 
         // Since we added digits from right to left, the list is backwards. Reverse it.
