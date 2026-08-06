468c836da56681f8b7792f777aa7473b26d699aa
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-06 14:41:37 +0530

    Time: 0 ms (100%), Space: 43.4 MB (22.69%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.java
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.java
@@ -18,46 +18,45 @@ class Solution {
         Queue<TreeNode> q = new LinkedList<>();
         q.offer(root);
 
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
             int size = q.size();
 
             boolean foundX = false;
             boolean foundY = false;
 
-            for(int i = 0; i < size; i++) {
+            for (int i = 0; i < size; i++) {
                 TreeNode curr = q.poll();
 
-                if(curr.val == x) {
-                        foundX = true;
-                    }
+                if (curr.val == x) {
+                    foundX = true;
+                }
 
-                    if(curr.val == y) {
-                        foundY = true;
-                    }
-
+                if (curr.val == y) {
+                    foundY = true;
+                }
 
                 // Check if Children are Siblings
-                if(curr.left != null && curr.right != null) {
-                    if((curr.left.val == x && curr.right.val == y) || (curr.right.val == x && curr.left.val == y)) {
+                if (curr.left != null && curr.right != null) {
+                    if ((curr.left.val == x && curr.right.val == y) || (curr.right.val == x && curr.left.val == y)) {
                         return false;
                     }
                 }
 
                 // Check Whether x or y Exists in This Level
-                if(curr.left != null) {
+                if (curr.left != null) {
                     q.offer(curr.left);
                 }
 
-                if(curr.right != null) {
+                if (curr.right != null) {
                     q.offer(curr.right);
                 }
             }
 
-            if(foundX && foundY) {
+            if (foundX && foundY) {
                 return true;
             }
 
-            if(foundX || foundY) {
+            if (foundX || foundY) {
                 return false;
             }
         }
