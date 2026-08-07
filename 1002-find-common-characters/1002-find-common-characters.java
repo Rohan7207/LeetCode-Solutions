a8a656cf422141e76a0a3689c3d3383b0d460dad
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-07 14:51:26 +0530

    Time: 3 ms (93.27%), Space: 44.4 MB (81.96%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.java
+++ 1002-find-common-characters/1002-find-common-characters.java
@@ -6,27 +6,27 @@ class Solution {
         List<String> res = new ArrayList<>();
 
         // Initialize commonCharacterCounts with the characters from the first word
-        for(char ch : words[0].toCharArray()) {
+        for (char ch : words[0].toCharArray()) {
             commonCharacterCounts[ch - 'a']++;
         }
 
-        for(int i = 1; i < wordSize; i++) {
+        for (int i = 1; i < wordSize; i++) {
             Arrays.fill(currentCharacterCounts, 0);
 
             // Count characters in the current word
-            for(char ch : words[i].toCharArray()) {
+            for (char ch : words[i].toCharArray()) {
                 currentCharacterCounts[ch - 'a']++;
             }
 
             // Update the common character counts to keep the minimum counts
-            for(int letter = 0; letter < 26; letter++) {
-                commonCharacterCounts[letter] = Math.min(commonCharacterCounts[letter],             currentCharacterCounts[letter]);
+            for (int letter = 0; letter < 26; letter++) {
+                commonCharacterCounts[letter] = Math.min(commonCharacterCounts[letter], currentCharacterCounts[letter]);
             }
         }
 
         // Collect the common characters based on the final counts
-        for(int letter = 0; letter < 26; letter++) {
-            for(int commonCount = 0; commonCount < commonCharacterCounts[letter]; commonCount++) {
+        for (int letter = 0; letter < 26; letter++) {
+            for (int commonCount = 0; commonCount < commonCharacterCounts[letter]; commonCount++) {
                 res.add(String.valueOf((char) (letter + 'a')));
             }
         }
