d35896b169e34314f8d78febf4f5cd35e2d55511
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-09 20:43:20 +0530

    Time: 2 ms (55.53%), Space: 55.5 MB (43.02%) - LeetHub

---
+++ 1013-partition-array-into-three-parts-with-equal-sum/1013-partition-array-into-three-parts-with-equal-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean canThreePartsEqualSum(int[] arr) {
+        int totalSum = 0;
+
+        for(int num : arr) {
+            totalSum += num;
+        }
+
+        if(totalSum % 3 != 0) return false;
+
+        int target = totalSum / 3;
+        int count = 0;
+        int currSum = 0;
+
+        for(int num : arr) {
+            currSum += num;
+
+            if(currSum == target) {
+                currSum = 0;
+                count++;
+            }
+        }
+
+        return count >= 3;
+    }
+}
