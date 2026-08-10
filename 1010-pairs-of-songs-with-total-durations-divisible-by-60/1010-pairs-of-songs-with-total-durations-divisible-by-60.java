81438e1bda5e75c8bf88c698024d6f9b3cfa8f42
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-10 14:36:09 +0530

    Time: 3 ms (91.57%), Space: 50 MB (87%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
@@ -3,7 +3,7 @@ class Solution {
         int[] rem = new int[60];
         int count = 0;
 
-        for(int i = 0; i < time.length; i++) {
+        for (int i = 0; i < time.length; i++) {
             int r = time[i] % 60;
             int needed = (60 - r) % 60;
 
