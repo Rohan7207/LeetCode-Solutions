52bc296996c69c982681607f089cdf2b9434cedb
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-12 15:40:24 +0530

    Time: 0 ms (100%), Space: 42.8 MB (90.35%) - LeetHub

---
+++ 1023-camelcase-matching/1023-camelcase-matching.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<Boolean> camelMatch(String[] queries, String pattern) {
+        List<Boolean> ans = new ArrayList<>();
+        int n = pattern.length();
+
+        for(String query : queries) {
+            int i = 0;
+            int j = 0;
+            int m = query.length();
+            
+            while(i < m) {
+                if(j < n && query.charAt(i) == pattern.charAt(j)) {
+                    i++;
+                    j++;
+                } else if(Character.isLowerCase(query.charAt(i))) {
+                    i++;
+                } else {
+                    break;  // Reject unmatched uppercase letters
+                }
+            }
+
+            ans.add(i == m && j == n);
+        }
+
+        return ans;
+    }
+}
