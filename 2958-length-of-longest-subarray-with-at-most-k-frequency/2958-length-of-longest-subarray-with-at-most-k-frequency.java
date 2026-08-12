cf04744112bcbc9435616b670200761518766496
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-12 14:21:52 +0530

    Time: 68 ms (50.38%), Space: 101.6 MB (15.98%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
@@ -4,10 +4,10 @@ class Solution {
         int ans = 0;
         int left = 0;
 
-        for(int right = 0; right < nums.length; right++) {
+        for (int right = 0; right < nums.length; right++) {
             freqMap.put(nums[right], freqMap.getOrDefault(nums[right], 0) + 1);
 
-            while(freqMap.get(nums[right]) > k) {
+            while (freqMap.get(nums[right]) > k) {
                 freqMap.put(nums[left], freqMap.get(nums[left]) - 1);
                 left++;
             }
