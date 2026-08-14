1eb943203a082b3a6d70d34c7648ae462664e91b
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-14 18:54:52 +0530

    Time: 4 ms (80.45%), Space: 43.4 MB (46.21%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[] temp = heights.clone();
+        Arrays.sort(heights);
+        int count = 0;
+
+        for(int i = 0; i < temp.length; i++) {
+            if(temp[i] != heights[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
