08eac88478c832826c46228b8c43b8007ed241f7
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-15 13:54:29 +0530

    Time: 0 ms (100%), Space: 43.1 MB (16.27%) - LeetHub

--- 1078-occurrences-after-bigram/1078-occurrences-after-bigram.java
+++ 1078-occurrences-after-bigram/1078-occurrences-after-bigram.java
@@ -4,12 +4,11 @@ class Solution {
         List<String> ans = new ArrayList<>();
         int n = words.length;
 
-        for(int i = 0; i < n - 2; i++) {
-            if(first.equals(words[i]) && second.equals(words[i + 1])) {
+        for (int i = 0; i < n - 2; i++) {
+            if (first.equals(words[i]) && second.equals(words[i + 1])) {
                 ans.add(words[i + 2]);
             }
         }
-        
         return ans.toArray(new String[0]);
     }
 }
