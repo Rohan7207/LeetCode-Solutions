4f7e59774ea64797c27dabb0fe92eddcb6db8eed
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-15 14:12:59 +0530

    Time: 0 ms (100%), Space: 47.1 MB (85.77%) - LeetHub

--- 1080-insufficient-nodes-in-root-to-leaf-paths/1080-insufficient-nodes-in-root-to-leaf-paths.java
+++ 1080-insufficient-nodes-in-root-to-leaf-paths/1080-insufficient-nodes-in-root-to-leaf-paths.java
@@ -19,14 +19,14 @@ class Solution {
     }
 
     private TreeNode dfs(TreeNode root, int limit, int sum) {
-        if(root == null) {
+        if (root == null) {
             return null;
         }
 
         sum += root.val;
 
         // Leaf node
-        if(root.left == null && root.right == null) {
+        if (root.left == null && root.right == null) {
             return sum < limit ? null : root;
         }
 
@@ -34,7 +34,7 @@ class Solution {
         root.right = dfs(root.right, limit, sum);
 
         // Both paths through this node were insufficient
-        if(root.left == null && root.right == null) {
+        if (root.left == null && root.right == null) {
             return null;
         }
 
