56ad24e18950098017c09e6ce1282a056a9271bc
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-16 14:11:25 +0530

    Time: 4 ms (75.68%), Space: 114.7 MB (82.43%) - LeetHub

--- 2029-stone-game-ix/2029-stone-game-ix.java
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -2,11 +2,11 @@ class Solution {
     public boolean stoneGameIX(int[] stones) {
         int[] count = new int[3];
 
-        for(int stone : stones) {
+        for (int stone : stones) {
             count[stone % 3]++;
         }
 
-        if(count[0] % 2 == 0) {
+        if (count[0] % 2 == 0) {
             return count[1] > 0 && count[2] > 0;
         }
 
