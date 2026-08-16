511c14b1957e2ba8e200d961d880aa000a7aba81
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-16 14:59:29 +0530

    Time: 12 ms (95.69%), Space: 48.1 MB (6.52%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -2,37 +2,37 @@ class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
         int n = grid.length;
 
-        if(grid[0][0] != 0 || grid[n - 1][n - 1] != 0) {
+        if (grid[0][0] != 0 || grid[n - 1][n - 1] != 0) {
             return -1;
         }
 
         Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {0, 0, 1});
+        q.offer(new int[] { 0, 0, 1 });
 
         grid[0][0] = 1;
 
         int[][] dirs = {
-            {-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}
+                { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, -1 }, { 0, 1 }, { 1, -1 }, { 1, 0 }, { 1, 1 }
         };
 
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
             int[] curr = q.poll();
 
             int row = curr[0];
             int col = curr[1];
             int dist = curr[2];
 
-            if(row == n - 1 && col == n - 1) {
+            if (row == n - 1 && col == n - 1) {
                 return dist;
             }
 
-            for(int[] dir : dirs) {
+            for (int[] dir : dirs) {
                 int nrow = row + dir[0];
                 int ncol = col + dir[1];
 
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < n && grid[nrow][ncol] == 0) {
+                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < n && grid[nrow][ncol] == 0) {
                     grid[nrow][ncol] = 1;
-                    q.offer(new int[] {nrow, ncol, dist + 1});
+                    q.offer(new int[] { nrow, ncol, dist + 1 });
                 }
             }
         }
