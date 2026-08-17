f8af51bdecbb77b73ebbc3db103b820c152a1e05
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-17 14:11:50 +0530

    Time: 1 ms (97.63%), Space: 47.2 MB (67.46%) - LeetHub

--- 1093-statistics-from-a-large-sample/1093-statistics-from-a-large-sample.java
+++ 1093-statistics-from-a-large-sample/1093-statistics-from-a-large-sample.java
@@ -10,13 +10,13 @@ class Solution {
         int mode = 0;
 
         // Find minimum, maximum, mean and mode
-        for(int i = 0; i < 256; i++) {
-            if(count[i] > 0) {
+        for (int i = 0; i < 256; i++) {
+            if (count[i] > 0) {
                 totalCount += count[i];
                 totalSum += (long) i * count[i];
 
                 // First value with frequency > 0
-                if(minimum == -1) {
+                if (minimum == -1) {
                     minimum = i;
                 }
 
@@ -24,7 +24,7 @@ class Solution {
                 maximum = i;
 
                 // Value with highest frequency
-                if(count[i] > maxFreq) {
+                if (count[i] > maxFreq) {
                     maxFreq = count[i];
                     mode = i;
                 }
@@ -34,21 +34,21 @@ class Solution {
         double mean = (double) totalSum / totalCount;
 
         // Positions of the two middle elements
-        int pos1 = (totalCount - 1) / 2;   // Even
-        int pos2 = totalCount / 2;   // Odd
+        int pos1 = (totalCount - 1) / 2; // Even
+        int pos2 = totalCount / 2; // Odd
 
         int middle1 = -1;
         int middle2 = -1;
         int cumulative = 0;
 
-        for(int i = 0; i < 256; i++) {
+        for (int i = 0; i < 256; i++) {
             cumulative += count[i];
 
-            if(middle1 == -1 && cumulative > pos1) {
+            if (middle1 == -1 && cumulative > pos1) {
                 middle1 = i;
             }
 
-            if(middle2 == -1 && cumulative > pos2) {
+            if (middle2 == -1 && cumulative > pos2) {
                 middle2 = i;
                 break;
             }
@@ -56,6 +56,6 @@ class Solution {
 
         double median = (middle1 + middle2) / 2.0;
 
-        return new double[] {minimum, maximum, mean, median, mode};
+        return new double[] { minimum, maximum, mean, median, mode };
     }
 }
