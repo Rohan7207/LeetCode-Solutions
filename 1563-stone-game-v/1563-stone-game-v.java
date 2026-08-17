e168e40c9d88c0d222bd7a1147ae1d4e37d841c3
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-17 13:42:08 +0530

    Time: 222 ms (91.11%), Space: 47.8 MB (49.28%) - LeetHub

--- 1563-stone-game-v/1563-stone-game-v.java
+++ 1563-stone-game-v/1563-stone-game-v.java
@@ -1,9 +1,55 @@
 class Solution {
 
-    int[][] dp;
+    int[][] memo;
+    int[] prefixSum;
 
     public int stoneGameV(int[] stoneValue) {
         int n = stoneValue.length;
+
+        memo = new int[n][n];
+        prefixSum = new int[n + 1];
+
+        // Compute prefix sums for O(1) subarray sum queries
+        // 0, 6, 8, 11, 15, 20, 25
+        for(int i = 0; i < n; i++) {
+            prefixSum[i + 1] = prefixSum[i] + stoneValue[i];
+        }
+
+        return solve(0, n - 1);
+    }   
+
+    private int solve(int left, int right) {
+        // Base case: only one stone left, game ends (no further score)
+        if(left == right) {
+            return 0;
+        }
+
+        if(memo[left][right] != 0) {
+            return memo[left][right];
+        }
+
+        int maxScore = 0;
+
+        for(int i = left; i < right; i++) {
+            int leftSum = prefixSum[i + 1] - prefixSum[left];
+            int rightSum = prefixSum[right + 1] - prefixSum[i + 1];
+
+            if(leftSum < rightSum) {
+                maxScore = Math.max(maxScore, leftSum + solve(left, i));
+            } else if(leftSum > rightSum) {
+                maxScore = Math.max(maxScore, rightSum + solve(i + 1, right));
+            } else {
+                maxScore = Math.max(maxScore, leftSum + Math.max(solve(left, i), solve(i + 1, right)));
+            }
+        }
+
+        memo[left][right] = maxScore;
+        return maxScore;
+    }
+}
+
+/*
+     int n = stoneValue.length;
         dp = new int[n][n];
 
         return dfs(stoneValue, 0, n - 1);
@@ -40,4 +86,4 @@ class Solution {
 
         return dp[left][right];
     }
-}
+*/
