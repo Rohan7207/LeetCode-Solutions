f7c8233e697929a38825459b35da8ef5d71c2855
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-17 13:25:55 +0530

    Time: 278 ms (29.09%), Space: 47.6 MB (76.68%) - LeetHub

--- 1563-stone-game-v/1563-stone-game-v.java
+++ 1563-stone-game-v/1563-stone-game-v.java
@@ -10,30 +10,31 @@ class Solution {
     }
 
     private int dfs(int[] stoneValue, int left, int right) {
-        if(left == right) {
+        if (left == right) {
             return 0;
         }
 
-        if(dp[left][right] != 0) {
+        if (dp[left][right] != 0) {
             return dp[left][right];
         }
 
         int sum = 0;
-        for(int i = left; i <= right; i++) {
+        for (int i = left; i <= right; i++) {
             sum += stoneValue[i];
         }
 
         int suml = 0;
-        for(int i = left; i < right; i++) {
+        for (int i = left; i < right; i++) {
             suml += stoneValue[i];
             int sumr = sum - suml;
 
-            if(suml < sumr) {
+            if (suml < sumr) {
                 dp[left][right] = Math.max(dp[left][right], dfs(stoneValue, left, i) + suml);
-            } else if(suml > sumr) {
+            } else if (suml > sumr) {
                 dp[left][right] = Math.max(dp[left][right], dfs(stoneValue, i + 1, right) + sumr);
             } else {
-                dp[left][right] = Math.max(dp[left][right], Math.max(dfs(stoneValue, left, i), dfs(stoneValue, i + 1, right)) + suml);
+                dp[left][right] = Math.max(dp[left][right],
+                        Math.max(dfs(stoneValue, left, i), dfs(stoneValue, i + 1, right)) + suml);
             }
         }
 
