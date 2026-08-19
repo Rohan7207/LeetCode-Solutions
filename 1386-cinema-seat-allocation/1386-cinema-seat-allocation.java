e8f961d688739b4e949c08a0267bd79d8a5144bc
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-19 15:23:46 +0530

    Time: 30 ms (40.26%), Space: 52.2 MB (69.47%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
@@ -2,7 +2,7 @@ class Solution {
     public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
         Map<Integer, Set<Integer>> reserved = new HashMap<>();
 
-        for(int[] seats : reservedSeats) {
+        for (int[] seats : reservedSeats) {
             int row = seats[0];
             int col = seats[1];
 
@@ -10,16 +10,16 @@ class Solution {
         }
 
         int groups = 0;
-        for(Map.Entry<Integer, Set<Integer>> entry : reserved.entrySet()) {
+        for (Map.Entry<Integer, Set<Integer>> entry : reserved.entrySet()) {
             Set<Integer> seats = entry.getValue();
 
             boolean left = canSit(seats, 2);
             boolean middle = canSit(seats, 4);
             boolean right = canSit(seats, 6);
 
-            if(left && right) {
+            if (left && right) {
                 groups += 2;
-            } else if(left || middle || right) {
+            } else if (left || middle || right) {
                 groups++;
             }
         }
@@ -27,11 +27,11 @@ class Solution {
         groups += (n - reserved.size()) * 2;
 
         return groups;
-    }   
+    }
 
     private boolean canSit(Set<Integer> seats, int start) {
-        for(int i = start; i < start + 4; i++) {
-            if(seats.contains(i)) {
+        for (int i = start; i < start + 4; i++) {
+            if (seats.contains(i)) {
                 return false;
             }
         }
