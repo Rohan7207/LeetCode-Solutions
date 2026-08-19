8a9d938f45a583ee8c294fa396ad795144b99f41
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-19 15:47:58 +0530

    Time: 8 ms (62.96%), Space: 47.2 MB (5.59%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,6 +1,37 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] copy = nums.clone();
+        Arrays.sort(copy);
         int n = nums.length;
+
+        for(int i = 0; i < n; i++) {
+            map.putIfAbsent(copy[i], i);
+        }
+
+        for(int i = 0; i < n; i++) {
+            copy[i] = map.get(nums[i]);
+        }
+
+        return copy;
+    }
+}
+
+/*
+    Create a copy of the input array. copy = [8,1,2,2,3]
+Sort the copy array. copy = [1,2,2,3,8]
+Fill the map: number => count (where count is an index in sorted array, so first number with index 0 has 0 numbers less than it, index 1 has 1 number less, etc). We update only first time we enocunter the number so that way we skip duplicates.
+map[1]=>0
+map[2]=>1
+map[3]=>3
+map[8]=>4
+We re-use our copy array to get our result, we iterate over original array, and get counts from the map.
+[4,0,1,1,3]
+*/
+
+/*
+    Brute Force: 
+          int n = nums.length;
         int[] ans = new int[n];
 
         for(int i = 0; i < n; i++) {
@@ -19,5 +50,4 @@ class Solution {
         }
 
         return ans;
-    }
-}
+*/
