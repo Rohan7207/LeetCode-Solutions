ea34df77b3c194316daa6bf72d38d93852a2d949
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-21 15:12:24 +0530

    Time: 6 ms (87.17%), Space: 47.4 MB (83.44%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
@@ -1,6 +1,38 @@
 class Solution {
     public int countCharacters(String[] words, String chars) {
-        int[] countOfChars = new int[26];
+        int[] freq = new int[26];
+
+        for(char c : chars.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        int ans = 0;
+        for(String word : words) {
+            int[] need = new int[26];
+
+            for(char c : word.toCharArray()) {
+                need[c - 'a']++;
+            }
+
+            boolean flag = true;
+            for(int i = 0; i < 26; i++) {
+                if(need[i] > freq[i]) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if(flag) {
+                ans += word.length();
+            }
+        }
+
+        return ans;
+    }
+}
+
+/*
+    int[] countOfChars = new int[26];
 
         for(char c : chars.toCharArray()) {
             countOfChars[c - 'a']++;
@@ -27,5 +59,4 @@ class Solution {
         }
 
         return ans;
-    }
-}
+*/
