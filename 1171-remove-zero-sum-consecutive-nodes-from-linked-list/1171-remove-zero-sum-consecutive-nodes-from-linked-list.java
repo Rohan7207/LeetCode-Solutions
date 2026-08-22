c7f61c3330ecb20e272963e32aa6707cc81351f1
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-22 15:22:38 +0530

    Time: 2 ms (95.84%), Space: 45.7 MB (50.04%) - LeetHub

--- 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.java
+++ 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.java
@@ -10,7 +10,53 @@
  */
 class Solution {
     public ListNode removeZeroSumSublists(ListNode head) {
-        ListNode front = new ListNode(0);
+        ListNode front = new ListNode(0, head);
+        ListNode current = front;
+
+        int prefixSum = 0;
+        Map<Integer, ListNode> prefixSumToNode = new HashMap<>();
+        prefixSumToNode.put(0, front);
+
+        // Calculate the prefix sum for each node and add to the hashmap
+        // Duplicate prefix sum values will be replaced
+        while(current != null) {
+            prefixSum += current.val;
+            prefixSumToNode.put(prefixSum, current);
+
+            current = current.next;
+        }
+
+        // Reset prefix sum and current
+        prefixSum = 0;
+        current = front;
+
+        // Delete zero sum consecutive sequences 
+        // by setting node before sequence to node after
+        while(current != null) {
+            prefixSum += current.val;
+
+            current.next = prefixSumToNode.get(prefixSum).next;
+            current = current.next;
+        }
+
+        return front.next;
+    }
+}
+
+/*
+    The crucial insight is that the prefix sum from the front node to node A will be equal to the sum from the front node to node B if and only if the sum from node A.next to node B is 0.
+    
+    https://leetcode.com/problems/remove-zero-sum-consecutive-nodes-from-linked-list/Figures/1171/1171ExampleA3.png$0
+    The prefix sum at the end of this consecutive sequence, 5, is the same as the prefix sum before the sequence.
+
+This makes sense; a zero-sum consecutive sequence will have a prefix sum of zero. The prefix sum before and at the end of the sequence will be the same. When we encounter a prefix sum that we have seen before, we have discovered a zero-sum consecutive sequence.
+
+*/
+
+
+/*
+    Brute Force: O(n ^ 2) and O(1)
+    ListNode front = new ListNode(0);
         front.next = head;
         ListNode start = front;
 
@@ -34,5 +80,4 @@ class Solution {
         }
 
         return front.next;
-    }
-}
+*/
