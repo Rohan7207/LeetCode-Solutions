97c7cbf3244c356a6ba16a6aa09afe759879a383
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-22 14:26:42 +0530

    Time: 0 ms (100%), Space: 42.3 MB (91.26%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,17 +1,11 @@
 class Solution {
     public int addDigits(int num) {
-        if(num < 10) {
-            return num;
+        // Use digital root property: repeated digit summation of a positive integer is equivalent to 1 + (num - 1) % 9
+        if(num == 0) {
+            return 0;
         }
 
-        int sum = 0;
-
-        while(num > 0) {
-            sum += num % 10;
-            num /= 10;
-        }
-
-        return addDigits(sum);
+        return 1 + (num - 1) % 9;
     }
 }
 
@@ -29,4 +23,20 @@ class Solution {
         }
 
         return num;
+*/
+
+/*
+    Recursive appraoch:
+    if(num < 10) {
+            return num;
+        }
+
+        int sum = 0;
+
+        while(num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        return addDigits(sum);
 */
