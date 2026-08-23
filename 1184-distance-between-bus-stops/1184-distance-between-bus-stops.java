432926527bcac812f1bd78d43bd1adeb8ec7737b
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-23 13:44:11 +0530

    Time: 0 ms (100%), Space: 44.1 MB (77.78%) - LeetHub

--- 1184-distance-between-bus-stops/1184-distance-between-bus-stops.java
+++ 1184-distance-between-bus-stops/1184-distance-between-bus-stops.java
@@ -2,14 +2,14 @@ class Solution {
     public int distanceBetweenBusStops(int[] distance, int start, int destination) {
         int totalDistance = 0;
 
-        for(int d : distance) {
+        for (int d : distance) {
             totalDistance += d;
         }
 
         int i = start;
         int clockwiseDistance = 0;
-        
-        while(i != destination) {
+
+        while (i != destination) {
             clockwiseDistance += distance[i];
             i = (i + 1) % distance.length;
         }
