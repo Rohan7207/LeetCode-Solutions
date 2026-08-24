db3e8c9191b3f0d90a96ed0358180ce49bd35a7a
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-24 14:04:24 +0530

    Time: 3 ms (100%), Space: 83.1 MB (93.62%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.java
+++ 1872-stone-game-viii/1872-stone-game-viii.java
@@ -1,19 +1,16 @@
 class Solution {
     public int stoneGameVIII(int[] stones) {
         int n = stones.length;
-        int[] prefix = new int[n];
 
-        prefix[0] = stones[0];
-        for(int i = 1; i < n; i++) {
-            prefix[i] = prefix[i - 1] + stones[i];
+        for (int i = 1; i < n; i++) {
+            stones[i] += stones[i - 1];
         }
 
-        int[] dp = new int[n];
-        dp[n - 1] = prefix[n - 1];
-        for(int i = n - 2; i >= 1; i--) {
-            dp[i] = Math.max(dp[i + 1], prefix[i] - dp[i + 1]);
+        int best = stones[n - 1];
+        for (int i = n - 2; i >= 1; i--) {
+            best = Math.max(best, stones[i] - best);
         }
 
-        return dp[1];
+        return best;
     }
 }
