6e592b05ed58fd666728d9390474f2b9670aa5c9
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-24 14:32:17 +0530

    Time: 21 ms (72.01%), Space: 63.6 MB (91.89%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
@@ -6,15 +6,15 @@ class Solution {
         // Start with the maximum possible value so any first pair updates it
         int minDiff = Integer.MAX_VALUE;
 
-        for(int i = 0; i < arr.length - 1; i++) {
-            int currDiff = arr[i + 1] - arr[i];  // Since array is sorted, arr[i+1] >= arr[i]
+        for (int i = 0; i < arr.length - 1; i++) {
+            int currDiff = arr[i + 1] - arr[i]; // Since array is sorted, arr[i+1] >= arr[i]
 
-            if(currDiff < minDiff) {
+            if (currDiff < minDiff) {
                 // Found a smaller difference! Reset the list and update minDiff
                 minDiff = currDiff;
                 ans.clear();
                 ans.add(List.of(arr[i], arr[i + 1]));
-            } else if (currDiff == minDiff){
+            } else if (currDiff == minDiff) {
                 // Found another pair with the exact same minimum difference
                 ans.add(List.of(arr[i], arr[i + 1]));
             }
