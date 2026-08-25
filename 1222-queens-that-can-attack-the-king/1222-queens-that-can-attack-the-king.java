58617ffc917997b3c147feee5a60680508201b9f
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-25 14:52:42 +0530

    Time: 0 ms (100%), Space: 44.4 MB (27.13%) - LeetHub

---
+++ 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.java
@@ -0,0 +1,43 @@
+class Solution {
+    public List<List<Integer>> queensAttacktheKing(int[][] queens, int[] king) {
+        List<List<Integer>> ans = new ArrayList<>();
+        boolean[][] board = new boolean[8][8];
+
+        for(int[] queen : queens) {
+            board[queen[0]][queen[1]] = true;
+        }
+
+        int[][] directions = {
+            {-1, 0}, // up
+            {1, 0},  // down
+            {0, -1}, // left
+            {0, 1},  // right
+            {-1, -1}, // top-left
+            {-1, 1}, // top-right
+            {1, -1}, // down-left
+            {1, 1}   // down-right
+        };
+
+        for(int[] dir : directions) {
+            dfs(board, king[0], king[1], dir[0], dir[1], ans);
+        }
+
+        return ans;
+    }
+
+    private void dfs(boolean[][] board, int row, int col, int dr, int dc, List<List<Integer>> ans) {
+        row += dr;
+        col += dc;
+
+        if(row < 0 || row >= 8 || col < 0 || col >= 8) {
+            return;
+        }
+
+        if(board[row][col]) {
+            ans.add(Arrays.asList(row, col));
+            return;
+        }
+
+        dfs(board, row, col, dr, dc, ans);
+    }
+}
