c3324facd260aa08a6e26ff01871c3533ece767b
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-25 15:36:54 +0530

    Time: 0 ms (100%), Space: 45.1 MB (43.76%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.java
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.java
@@ -6,11 +6,11 @@ class Solution {
         int x2 = coordinates[1][0];
         int y2 = coordinates[1][1];
 
-        for(int i = 2; i < coordinates.length; i++) {
+        for (int i = 2; i < coordinates.length; i++) {
             int x3 = coordinates[i][0];
             int y3 = coordinates[i][1];
 
-            if(((y2 - y1) * (x3 - x1)) != ((y3 - y1) * (x2 - x1))) {
+            if (((y2 - y1) * (x3 - x1)) != ((y3 - y1) * (x2 - x1))) {
                 return false;
             }
         }
