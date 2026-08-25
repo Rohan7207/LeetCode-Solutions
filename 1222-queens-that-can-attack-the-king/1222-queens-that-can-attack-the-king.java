8238c55fae35d13891cabcbbba7d9e7231c8f968
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-25 14:53:18 +0530

    Time: 0 ms (100%), Space: 44.2 MB (58.14%) - LeetHub

--- 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.java
+++ 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.java
@@ -3,22 +3,22 @@ class Solution {
         List<List<Integer>> ans = new ArrayList<>();
         boolean[][] board = new boolean[8][8];
 
-        for(int[] queen : queens) {
+        for (int[] queen : queens) {
             board[queen[0]][queen[1]] = true;
         }
 
         int[][] directions = {
-            {-1, 0}, // up
-            {1, 0},  // down
-            {0, -1}, // left
-            {0, 1},  // right
-            {-1, -1}, // top-left
-            {-1, 1}, // top-right
-            {1, -1}, // down-left
-            {1, 1}   // down-right
+                { -1, 0 }, // up
+                { 1, 0 }, // down
+                { 0, -1 }, // left
+                { 0, 1 }, // right
+                { -1, -1 }, // top-left
+                { -1, 1 }, // top-right
+                { 1, -1 }, // down-left
+                { 1, 1 } // down-right
         };
 
-        for(int[] dir : directions) {
+        for (int[] dir : directions) {
             dfs(board, king[0], king[1], dir[0], dir[1], ans);
         }
 
@@ -29,11 +29,11 @@ class Solution {
         row += dr;
         col += dc;
 
-        if(row < 0 || row >= 8 || col < 0 || col >= 8) {
+        if (row < 0 || row >= 8 || col < 0 || col >= 8) {
             return;
         }
 
-        if(board[row][col]) {
+        if (board[row][col]) {
             ans.add(Arrays.asList(row, col));
             return;
         }
