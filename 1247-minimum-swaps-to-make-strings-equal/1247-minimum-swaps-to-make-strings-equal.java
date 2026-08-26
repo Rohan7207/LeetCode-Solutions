98080508081878ee9bbf2bac5138bd9384aab991
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-26 15:45:50 +0530

    Time: 1 ms (92.35%), Space: 42.7 MB (83.49%) - LeetHub

--- 1247-minimum-swaps-to-make-strings-equal/1247-minimum-swaps-to-make-strings-equal.java
+++ 1247-minimum-swaps-to-make-strings-equal/1247-minimum-swaps-to-make-strings-equal.java
@@ -3,22 +3,40 @@ class Solution {
         int xy = 0;
         int yx = 0;
 
-        for(int i = 0; i < s1.length(); i++) {
-            if(s1.charAt(i) == s2.charAt(i)) {
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) == s2.charAt(i)) {
                 continue;
             }
 
-            if(s1.charAt(i) == 'x') {
+            if (s1.charAt(i) == 'x') {
                 xy++;
             } else {
                 yx++;
             }
         }
 
-        if((xy + yx) % 2 == 1) {
+        if ((xy + yx) % 2 == 1) {
             return -1;
         }
 
         return xy / 2 + yx / 2 + (xy % 2) * 2;
     }
 }
+
+/*
+     public int minimumSwap(String s1, String s2) {
+        if(s1.length() != s2.length()) return -1;
+		int n = s1.length();
+        int x = 0 , y = 0;
+        
+        for(int i = 0 ; i < n ; i ++){
+            char c1 = s1.charAt(i) , c2 = s2.charAt(i);
+            if(c1 == 'x' && c2 == 'y') x++;
+            else if(c1 == 'y' && c2 == 'x') y++;
+        }
+        
+        if(x % 2 == 0 && y % 2 == 0) return x/2 + y/2;
+        else if(x % 2 == 1 && y % 2 == 1) return x/2 + y/2 + 2;
+        return -1;
+    }
+*/
