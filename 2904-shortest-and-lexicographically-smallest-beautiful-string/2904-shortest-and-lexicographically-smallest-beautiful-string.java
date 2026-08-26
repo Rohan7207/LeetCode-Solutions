443608e58bae6507878d8e71460090b8fb85373d
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-26 14:25:19 +0530

    Time: 2 ms (40.94%), Space: 44 MB (58.39%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
@@ -2,26 +2,26 @@ class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
         int totalOne = 0;
 
-        for(int i = 0; i < s.length(); i++) {
-            totalOne += s.charAt(i) - '0';    // If '1' returns 1 or '0' returns 0 according to ASCII
+        for (int i = 0; i < s.length(); i++) {
+            totalOne += s.charAt(i) - '0'; // If '1' returns 1 or '0' returns 0 according to ASCII
         }
 
-        if(totalOne < k) return "";
+        if (totalOne < k) return "";
 
         String ans = s;
         int left = 0;
         int cnt = 0;
-        for(int right = 0; right < s.length(); right++) {
+        for (int right = 0; right < s.length(); right++) {
             cnt += s.charAt(right) - '0';
 
-            while(cnt > k || s.charAt(left) == '0') {
+            while (cnt > k || s.charAt(left) == '0') {
                 cnt -= s.charAt(left++) - '0';
             }
 
-            if(cnt == k) {
+            if (cnt == k) {
                 String temp = s.substring(left, right + 1);
 
-                if(temp.length() < ans.length() || ((temp.length() == ans.length()) && temp.compareTo(ans) < 0)) {
+                if (temp.length() < ans.length() || ((temp.length() == ans.length()) && temp.compareTo(ans) < 0)) {
                     ans = temp;
                 }
             }
@@ -30,3 +30,7 @@ class Solution {
         return ans;
     }
 }
+
+/*
+    A substring is better if it has a shorter length, or if the lengths are equal and it is lexicographically smaller.
+*/
