ee37840cd765c1e58bc8b21f7b8bcffb0315b1c3
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-27 15:26:41 +0530

    Time: 3 ms (41.2%), Space: 46.7 MB (23.74%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -4,9 +4,9 @@ class Solution {
         int n = grid[0].length;
 
         int count = 0;
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(grid[i][j] == 0 && dfs(grid, i, j)) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 0 && dfs(grid, i, j)) {
                     count++;
                 }
             }
@@ -16,24 +16,24 @@ class Solution {
     }
 
     private boolean dfs(int[][] grid, int row, int col) {
-        if(row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) {
             return false;
         }
 
-        if(grid[row][col] == 1) {
+        if (grid[row][col] == 1) {
             return true;
         }
 
         grid[row][col] = 1;
         boolean isClosed = true;
 
-        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        int[][] dirs = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
 
-        for(int[] dir: dirs) {
+        for (int[] dir : dirs) {
             int nrow = row + dir[0];
             int ncol = col + dir[1];
 
-            if(!(dfs(grid, nrow, ncol))) {
+            if (!(dfs(grid, nrow, ncol))) {
                 isClosed = false;
             }
         }
