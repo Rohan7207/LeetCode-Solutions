622f7846b67f8211d7991e638972bbd0eb6145d2
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-28 14:02:23 +0530

    Time: 74 ms (19.05%), Space: 47.6 MB (33.33%) - LeetHub

--- 3734-lexicographically-smallest-palindromic-permutation-greater-than-target/3734-lexicographically-smallest-palindromic-permutation-greater-than-target.java
+++ 3734-lexicographically-smallest-palindromic-permutation-greater-than-target/3734-lexicographically-smallest-palindromic-permutation-greater-than-target.java
@@ -3,65 +3,65 @@ class Solution {
         int n = s.length();
 
         // Special case: length of 1
-        if(n == 1) {
+        if (n == 1) {
             return s.compareTo(target) > 0 ? s : "";
         }
 
         // Count the frequency of each character
         int[] count = new int[26];
-        for(char c : s.toCharArray()) {
+        for (char c : s.toCharArray()) {
             count[c - 'a']++;
         }
 
         // Check if it can form a palindrome and record the characters with odd occurrence
         String oddChar = "";
-        for(int i = 0; i < 26; i++) {
-            if(count[i] % 2 == 1) {
+        for (int i = 0; i < 26; i++) {
+            if (count[i] % 2 == 1) {
                 // More than one character appears an odd number of times, cannot form a palindrome
-                if(oddChar != "") {
+                if (oddChar != "") {
                     return "";
                 }
 
                 oddChar = String.valueOf((char) ('a' + i));
             }
 
-            count[i] /= 2;  // It takes only half the characters to construct the left half
+            count[i] /= 2; // It takes only half the characters to construct the left half
         }
 
         StringBuilder prefix = new StringBuilder();
 
         // Construct the left part of each digit greedily
-        for(int i = 0; i < n / 2; i++) {
+        for (int i = 0; i < n / 2; i++) {
             boolean found = false;
 
             // Try to place the smallest character in lexicographical order
-            for(int j = 0; j < 26; j++) {
-                if(count[j] == 0) {
+            for (int j = 0; j < 26; j++) {
+                if (count[j] == 0) {
                     continue;
                 }
 
                 count[j]--;
-                if(check(prefix.toString(), (char) ('a' + j), count, oddChar, target)) {
+                if (check(prefix.toString(), (char) ('a' + j), count, oddChar, target)) {
                     // If the constructed palindrome is greater than target, choose the character
                     prefix.append((char) ('a' + j));
                     found = true;
                     break;
                 } else {
-                    count[j]++;  // Not meeting the conditions, reset the counter
+                    count[j]++; // Not meeting the conditions, reset the counter
                 }
             }
 
             // Cannot construct a palindrome larger than target
-            if(!found) {
+            if (!found) {
                 return "";
             }
 
             // prefix is already greater than target
-            if(prefix.charAt(i) > target.charAt(i)) {
+            if (prefix.charAt(i) > target.charAt(i)) {
                 StringBuilder left = new StringBuilder(prefix);
 
-                for(int j = 0; j < 26; j++) {
-                    for(int k = 0; k < count[j]; k++) {
+                for (int j = 0; j < 26; j++) {
+                    for (int k = 0; k < count[j]; k++) {
                         left.append((char) ('a' + j));
                     }
                 }
@@ -83,8 +83,8 @@ class Solution {
         StringBuilder left = new StringBuilder(prefix);
         left.append(c);
 
-        for(int i = 25; i >= 0; i--) {
-            for(int k = 0; k < count[i]; k++) {
+        for (int i = 25; i >= 0; i--) {
+            for (int k = 0; k < count[i]; k++) {
                 left.append((char) ('a' + i));
             }
         }
