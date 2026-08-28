a30894202d08fd9b1e89419284812d87b7acfdae
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-28 14:42:27 +0530

    Time: 8 ms (80.04%), Space: 49.7 MB (52.82%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.java
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.java
@@ -2,11 +2,11 @@ class Solution {
     public int maxSumDivThree(int[] nums) {
         int[] f = { 0, Integer.MIN_VALUE, Integer.MIN_VALUE };
 
-        for(int num : nums) {
+        for (int num : nums) {
             int[] g = new int[3];
             System.arraycopy(f, 0, g, 0, 3);
 
-            for(int i = 0; i < 3; i++) {
+            for (int i = 0; i < 3; i++) {
                 g[(i + (num % 3)) % 3] = Math.max(g[(i + (num % 3)) % 3], f[i] + num);
             }
 
