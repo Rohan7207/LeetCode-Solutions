20f151abc6b37969a423b56607fca684e902dcb6
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-29 13:53:44 +0530

    Time: 108 ms (43.94%), Space: 235.2 MB (10.6%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.java
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.java
@@ -13,8 +13,8 @@ class Solution {
         groupToDeque.put(currGroup, new ArrayDeque<>());
         groupToDeque.get(currGroup).add(numsSorted[0]);
 
-        for(int i = 1; i < n; i++) {
-            if((numsSorted[i] - numsSorted[i - 1]) > limit) {
+        for (int i = 1; i < n; i++) {
+            if ((numsSorted[i] - numsSorted[i - 1]) > limit) {
                 currGroup++;
             }
 
@@ -22,9 +22,9 @@ class Solution {
 
             groupToDeque.putIfAbsent(currGroup, new ArrayDeque<>());
             groupToDeque.get(currGroup).add(numsSorted[i]);
-        }   
+        }
 
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             int group = numToGroup.get(nums[i]);
 
             nums[i] = groupToDeque.get(group).pollFirst();
