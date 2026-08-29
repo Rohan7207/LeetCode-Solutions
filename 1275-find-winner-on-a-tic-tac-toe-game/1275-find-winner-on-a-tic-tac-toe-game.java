82fb9078bc1ac6dcdcf7c008864253c4a02c1a37
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-29 14:33:17 +0530

    Time: 0 ms (100%), Space: 42.9 MB (83.37%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.java
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.java
@@ -2,11 +2,11 @@ class Solution {
     public String tictactoe(int[][] moves) {
         int[][] board = new int[3][3];
 
-        for(int i = 0; i < moves.length; i++) {
+        for (int i = 0; i < moves.length; i++) {
             int row = moves[i][0];
             int col = moves[i][1];
 
-            if(i % 2 == 0) {
+            if (i % 2 == 0) {
                 board[row][col] = 1;
             } else {
                 board[row][col] = 2;
@@ -14,26 +14,26 @@ class Solution {
         }
 
         // Row checking
-        for(int i = 0; i < 3; i++) {
-            if(board[i][0] != 0 && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] != 0 && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
                 return board[i][0] == 1 ? "A" : "B";
             }
         }
 
         // Column checking
-        for(int i = 0; i < 3; i++) {
-            if(board[0][i] != 0 && board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
+        for (int i = 0; i < 3; i++) {
+            if (board[0][i] != 0 && board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
                 return board[0][i] == 1 ? "A" : "B";
             }
         }
 
         // Diagonal Checking
         if ((board[0][0] != 0 && board[0][0] == board[1][1] && board[1][1] == board[2][2]) ||
-             (board[1][1] != 0 && board[1][1] == board[0][2] && board[1][1] == board[2][0])){
-                return board[1][1] == 1 ? "A" : "B";
+                (board[1][1] != 0 && board[1][1] == board[0][2] && board[1][1] == board[2][0])) {
+            return board[1][1] == 1 ? "A" : "B";
         }
 
-        if (moves.length == 9){
+        if (moves.length == 9) {
             return "Draw";
         }
 
