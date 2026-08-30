b194362ef4a20acb2a5466630dfb29ba62b9867c
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-30 15:02:37 +0530

    Enhance comments and clarify result string construction
    Added comments explaining the approach to solve the problem of minimum removal of parentheses for validity. Updated the result string construction to clarify the logic.

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,3 +1,34 @@
+// Problem: Minimum Remove to Make Valid Parenthesis
+// Link: https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
+// Difficulty: Medium
+
+// Approach:
+// Use Stack + Set of Invalid Indices.
+//
+// 1. Traverse the string character by character.
+//
+// 2. When we encounter '(':
+//    Store its index in the stack because it may need a matching ')'
+//
+// 3. When we encounter ')':
+//
+//      - If the stack is not empty, pop the matching '('.
+//      - If the stack is empty, this ')' has no matching '(',
+//        so mark its index for removal.
+//
+// 4. After traversal, any '(' indices remaining in the stack
+//    do not have matching ')' characters.
+//    Add all those indices to the removal set.
+//
+// 5. Traverse the string again and build the result by skipping
+//    every index present in `removeIndices`.
+//
+// 6. The remaining string contains only valid parentheses.
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+
 class Solution {
     public String minRemoveToMakeValid(String s) {
         Set<Integer> removeIndices = new HashSet<>();
@@ -21,7 +52,7 @@ class Solution {
             removeIndices.add(st.pop());
         }
 
-        //Add to result string
+        //Add characters which are not present in set to result string
         StringBuilder res = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
             if (!removeIndices.contains(i)) {
