dbd7f48e4f441db0f53c64a2c6d15a3635b69ba3
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-30 14:50:25 +0530

    Time: 14 ms (51.02%), Space: 59.7 MB (40.37%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,5 +1,37 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
+        return countAtMost(nums, k) - countAtMost(nums, k - 1);
+    }
+
+    private int countAtMost(int[] nums, int k) {
+        int n = nums.length;
+        int ans = 0;
+        int oddCount = 0;
+
+        int left = 0;
+        for(int right = 0; right < n; right++) {
+            if(nums[right] % 2 == 1) oddCount++;
+
+            while(oddCount > k) {
+                if(nums[left] % 2 == 1) oddCount--;
+
+                left++;
+            }
+
+            ans += right - left + 1;
+        }
+
+        return ans;
+    }
+}
+
+/*
+    However, for this problem, we need to calculate the number of subarrays with a sum exactly k (from Approach 1), not at most k. Observe that if we calculate the number of subarrays with sum at most k and at most k-1, their difference would give us the number of subarrays with sum exactly k.
+*/
+
+/*
+    O(n) and O(n)
+    public int numberOfSubarrays(int[] nums, int k) {
         int currSum = 0;
         int subArrays = 0;
 
@@ -18,4 +50,4 @@ class Solution {
 
         return subArrays;
     }
-}
+*/
