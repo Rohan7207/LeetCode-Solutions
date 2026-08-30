bea6497b729394ab5a303b125b9b3325637a756e
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-30 14:50:46 +0530

    Time: 13 ms (70.37%), Space: 59.7 MB (27.91%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -9,11 +9,13 @@ class Solution {
         int oddCount = 0;
 
         int left = 0;
-        for(int right = 0; right < n; right++) {
-            if(nums[right] % 2 == 1) oddCount++;
+        for (int right = 0; right < n; right++) {
+            if (nums[right] % 2 == 1)
+                oddCount++;
 
-            while(oddCount > k) {
-                if(nums[left] % 2 == 1) oddCount--;
+            while (oddCount > k) {
+                if (nums[left] % 2 == 1)
+                    oddCount--;
 
                 left++;
             }
