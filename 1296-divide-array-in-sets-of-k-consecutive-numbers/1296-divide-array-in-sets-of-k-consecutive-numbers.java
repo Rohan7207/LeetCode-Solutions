126d32e3b4ccee203c32ba8cd6387b53204e146b
Rohan Figredo <rohanfigredo@gmail.com>
2026-08-31 20:57:45 +0530

    Time: 93 ms (90.69%), Space: 129.5 MB (11.15%) - LeetHub

--- 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.java
+++ 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.java
@@ -1,26 +1,26 @@
 class Solution {
     public boolean isPossibleDivide(int[] nums, int k) {
-        if(nums.length % k != 0) {
+        if (nums.length % k != 0) {
             return false;
         }
 
         Arrays.sort(nums);
         Map<Integer, Integer> freq = new HashMap<>();
 
-        for(int num : nums) {
+        for (int num : nums) {
             freq.put(num, freq.getOrDefault(num, 0) + 1);
         }
 
-        for(int num : nums) {
+        for (int num : nums) {
             // num is used already
-            if(freq.get(num) == 0) {
+            if (freq.get(num) == 0) {
                 continue;
             }
 
-            for(int j = 0; j < k; j++) {
+            for (int j = 0; j < k; j++) {
                 int curr = num + j;
 
-                if(freq.getOrDefault(curr, 0) == 0) {
+                if (freq.getOrDefault(curr, 0) == 0) {
                     return false;
                 }
 
