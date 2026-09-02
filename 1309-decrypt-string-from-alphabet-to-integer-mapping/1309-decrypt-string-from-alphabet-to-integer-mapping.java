c8744a98e0a4c06e2b75a124be0428930f9f4710
Rohan Figredo <rohanfigredo@gmail.com>
2026-09-02 15:31:08 +0530

    Time: 1 ms (95.41%), Space: 43.1 MB (32.13%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.java
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.java
@@ -3,9 +3,9 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         int i = s.length() - 1;
 
-        while(i >= 0) {
+        while (i >= 0) {
             char ch = s.charAt(i);
-            if(ch == '#') {
+            if (ch == '#') {
                 String numStr = s.substring(i - 2, i);
                 int num = Integer.parseInt(numStr);
 
@@ -17,7 +17,7 @@ class Solution {
                 int num = ch - '0';
                 char letter = (char) (num + 96);
                 sb.append(letter);
-                
+
                 i--;
             }
         }
