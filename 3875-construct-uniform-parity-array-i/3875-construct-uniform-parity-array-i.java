c33390130a689a7cf2ac0655f3e8e31de72a66ea
Rohan Figredo <rohanfigredo@gmail.com>
2026-09-02 14:35:21 +0530

    Time: 1 ms (31.89%), Space: 45.1 MB (57.87%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        int oddCount = 0;
+        int evenCount = 0;
+
+        for(int num : nums1) {
+            if(num % 2 != 0) {
+                oddCount++;
+            } else {
+                evenCount++;
+            }
+        }
+
+        return oddCount > 0 || evenCount == nums1.length;
+    }
+}
