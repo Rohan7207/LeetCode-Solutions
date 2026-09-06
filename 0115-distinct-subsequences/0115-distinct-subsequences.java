60ee22c139c2a5c106504311ad128e237af28bed
Rohan Figredo <rohanfigredo@gmail.com>
2026-09-06 13:31:12 +0530

    Time: 19 ms (69.08%), Space: 54.2 MB (73.01%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -9,28 +9,33 @@ class Solution {
             Arrays.fill(row, -1);
         }
 
-        return solve(s, t, dp, m, n);
-    }
-
-    private int solve(String s, String t, int[][] dp, int m, int n) {
-        if(n == 0) {
-            return 1;   // Found one sequence
+        // Base 1
+        // if(n == 0) return 1;   // Found one sequence
+        // dp[m][0] = 1;
+        for(int i = 0; i <= m; i++) {
+            dp[i][0] = 1;
         }
 
-        if(m == 0) {
-            return 0;   
+        // Base 2
+        // if(m == 0) return 0;   
+        // dp[0][n] = 0;
+        for(int j = 1; j <= n; j++) {
+            dp[0][j] = 0;
         }
 
-        if(dp[m][n] != -1) {
-            return dp[m][n];
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(s.charAt(i - 1) == t.charAt(j - 1)) {
+                    // dp[m][n] = solve(s, t, dp, m - 1, n - 1) + solve(s, t, dp, m - 1, n);
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    // dp[m][n] = solve(s, t, dp, m - 1, n);
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
         }
 
-        if(s.charAt(m - 1) == t.charAt(n - 1)) {
-            dp[m][n] = solve(s, t, dp, m - 1, n - 1) + solve(s, t, dp, m - 1, n);
-        } else {
-            dp[m][n] = solve(s, t, dp, m - 1, n);
-        }
-
+        // return solve(s, t, dp, m, n);
         return dp[m][n];
     }
 }
@@ -74,6 +79,44 @@ class Solution {
 */
 
 /*
+    // Second Recusrion type by which we can easily convert to bottom up
+    public int numDistinct(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        for(int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+
+        return solve(s, t, dp, m, n);
+    }
+
+    private int solve(String s, String t, int[][] dp, int m, int n) {
+        if(n == 0) {
+            return 1;   // Found one sequence
+        }
+
+        if(m == 0) {
+            return 0;   
+        }
+
+        if(dp[m][n] != -1) {
+            return dp[m][n];
+        }
+
+        if(s.charAt(m - 1) == t.charAt(n - 1)) {
+            dp[m][n] = solve(s, t, dp, m - 1, n - 1) + solve(s, t, dp, m - 1, n);
+        } else {
+            dp[m][n] = solve(s, t, dp, m - 1, n);
+        }
+
+        return dp[m][n];
+    }
+*/
+
+/*
     s = rabbbit  t = rabbit
         i            j      -> match
          i            j     -> match
@@ -113,4 +156,35 @@ class Solution {
     } else {
         solve(s, t, m - 1, n);
     }
+
+    Convertion of second type to bottom up approach: dp[m][n] = x; x is count of subsequence of s == t
+
+    // if(n == 0) return 1:
+        for(i = 0; i <= n; i++) {      dp = 1 0 0 0 0
+            dp[i][0] = 1;                   0
+        }                                   0
+                                            0
+    // if(m == 0) return 0;                 0
+        for(j = 1; j <= m; j++) {
+            dp[0][j] = 0;
+        }
+
+    // if(s.charAt(m - 1) == t.charAt(n - 1)) {
+    //     solve(s, t, m - 1, n - 1) + solve(s, t, m - 1, n);
+    // } else {
+    //     solve(s, t, m - 1, n);
+    // }:
+
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+    // solve(s, t, m, n) : function call,
+     return dp[m][n];
 */  
