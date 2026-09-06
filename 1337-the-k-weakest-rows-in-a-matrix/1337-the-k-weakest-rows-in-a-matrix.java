09b0fba1c21895269b06c416899e54cf6f4c1880
Rohan Figredo <rohanfigredo@gmail.com>
2026-09-06 15:42:03 +0530

    Time: 3 ms (64.51%), Space: 46.6 MB (47.08%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -4,16 +4,16 @@ class Solution {
         int n = mat[0].length;
         Integer[][] rows = new Integer[m][2];
 
-        for(int i = 0; i < m; i++) {
+        for (int i = 0; i < m; i++) {
             int count = 0;
-            for(int j = 0; j < n; j++) {
-                if(mat[i][j] == 1) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1) {
                     count++;
                 }
             }
 
-            rows[i][0] = count;  // count of soldiers
-            rows[i][1] = i;      // row index
+            rows[i][0] = count; // count of soldiers
+            rows[i][1] = i; // row index
         }
 
         Arrays.sort(rows, (a, b) -> {
@@ -21,12 +21,12 @@ class Solution {
                 return a[0] - b[0]; // fewer soldiers first
             }
 
-            return a[1] - b[1];     // smaller index first
+            return a[1] - b[1]; // smaller index first
         });
 
         int[] res = new int[k];
 
-        for(int i = 0; i < k; i++) {
+        for (int i = 0; i < k; i++) {
             res[i] = rows[i][1];
         }
 
