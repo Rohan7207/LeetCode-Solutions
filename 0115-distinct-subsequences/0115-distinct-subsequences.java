9051ef7939c1754c1714013ead121015eaff8707
Rohan Figredo <rohanfigredo@gmail.com>
2026-09-06 13:13:06 +0530

    Time: 20 ms (53.25%), Space: 54 MB (77.76%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -9,6 +9,44 @@ class Solution {
             Arrays.fill(row, -1);
         }
 
+        return solve(s, t, dp, m, n);
+    }
+
+    private int solve(String s, String t, int[][] dp, int m, int n) {
+        if(n == 0) {
+            return 1;   // Found one sequence
+        }
+
+        if(m == 0) {
+            return 0;   
+        }
+
+        if(dp[m][n] != -1) {
+            return dp[m][n];
+        }
+
+        if(s.charAt(m - 1) == t.charAt(n - 1)) {
+            dp[m][n] = solve(s, t, dp, m - 1, n - 1) + solve(s, t, dp, m - 1, n);
+        } else {
+            dp[m][n] = solve(s, t, dp, m - 1, n);
+        }
+
+        return dp[m][n];
+    }
+}
+
+/*
+    // First Recursion type:
+    public int numDistinct(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        for(int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+
         return solve(s, t, dp, 0, 0);
     }
 
@@ -33,7 +71,7 @@ class Solution {
 
         return dp[i][j];
     }
-}
+*/
 
 /*
     s = rabbbit  t = rabbit
@@ -42,6 +80,7 @@ class Solution {
           i            j    -> match, now we can either choose this and explore or check next state 
                                 i + 1  
 
+    // Recursion:
     rough idea: solve(s, t, dp, 0, 0) where dp[m][n]
                             i, j
 
@@ -61,4 +100,17 @@ class Solution {
         }
     if both charcters match we have two choices either move both i and j and explore or else move  
         only i and explore future
-*/
+
+    Second Recursion type where we use m and n and when we convert recursion to bottom =up it becomes easy to convert just m to i and n to j:
+
+    solve(s, t, m, n): With memoization,
+    if(n == 0) return 1;
+
+    if(m == 0) return 0;
+
+    if(s.charAt(m - 1) == t.charAt(n - 1)) {
+        solve(s, t, m - 1, n - 1) + solve(s, t, m - 1, n);
+    } else {
+        solve(s, t, m - 1, n);
+    }
+*/  
