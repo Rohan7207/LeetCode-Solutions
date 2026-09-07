4d5cbdeb70d238f1413f54c97fd20d88b8826bfe
Rohan Figredo <rohanfigredo@gmail.com>
2026-09-07 16:03:59 +0530

    Time: 40 ms (40.53%), Space: 88.1 MB (37.27%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minSetSize(int[] arr) {
+        Map<Integer, Integer> freq = new HashMap<>();
+
+        for(int num : arr) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+
+        // Extract frequencies and sort them in descending order
+        List<Integer> frequencies = new ArrayList<>(freq.values());
+        frequencies.sort(Collections.reverseOrder());
+
+        int target = arr.length / 2;
+        int removedElements = 0;
+        int setCounts = 0;
+        for(int count : frequencies) {
+            removedElements += count;
+            setCounts++;
+
+            // As soon as we've removed at least half the array, we are done
+            if(removedElements >= target) {
+                break;
+            }
+        }
+
+        return setCounts;
+    }
+}
