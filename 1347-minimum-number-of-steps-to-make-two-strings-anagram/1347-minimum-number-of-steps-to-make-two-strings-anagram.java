62c64a7913236bbe60ff1562a85c6995f2723b8f
Rohan Figredo <rohanfigredo@gmail.com>
2026-09-09 22:03:13 +0530

    Document problem and approach for anagram steps
    Added problem statement and approach for the minimum number of steps to make two strings anagram.

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -1,3 +1,24 @@
+// Problem: Minimum Number of Steps to Make Two Strings Anagram
+// Link: https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram/
+// Difficulty: Medium
+
+// Approach:
+// Use Frequency Counting.
+//
+// 1. Count the frequency of every character in s.
+// 2. Subtract the frequency of every character in t.
+// 3. A positive frequency means s contains extra copies of that
+//    character that must be removed.
+// 4. Add all positive frequencies to get the minimum number of steps.
+//
+// Why does this work?
+// Since we can delete characters from s, only the characters that
+// appear more times in s than in t need to be removed.
+
+// Time Complexity: O(n + m)
+// Space Complexity: O(1)  // only 26 characters
+
+
 class Solution {
     public int minSteps(String s, String t) {
         int[] freq = new int[26];
