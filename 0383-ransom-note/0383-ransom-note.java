1fe5948c1025f5436c6bb0f9bcd97d4fe168f2dd
Rohan Figredo <rohanfigredo@gmail.com>
2026-09-10 16:37:35 +0530

    Time: 2 ms (56.95%), Space: 46.4 MB (67.9%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -2,13 +2,13 @@ class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         int[] freq = new int[26];
 
-        for(char ch : magazine.toCharArray()) {
+        for (char ch : magazine.toCharArray()) {
             freq[ch - 'a']++;
         }
 
-        for(int i = 0; i < ransomNote.length(); i++) {
+        for (int i = 0; i < ransomNote.length(); i++) {
             char ch = ransomNote.charAt(i);
-            if(freq[ch - 'a'] <= 0) {
+            if (freq[ch - 'a'] <= 0) {
                 return false;
             }
 
