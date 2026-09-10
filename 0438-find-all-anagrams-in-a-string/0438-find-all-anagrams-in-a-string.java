9c774b567da8d04f3c1e2f2140802a275aff6a7e
Rohan Figredo <rohanfigredo@gmail.com>
2026-09-10 17:06:46 +0530

    Time: 6 ms (97.23%), Space: 46.8 MB (57.6%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -3,11 +3,11 @@ class Solution {
         List<Integer> ans = new ArrayList<>();
         int[] freqP = new int[26];
 
-        if(p.length() > s.length()) {
+        if (p.length() > s.length()) {
             return ans;
         }
 
-        for(char ch : p.toCharArray()) {
+        for (char ch : p.toCharArray()) {
             freqP[ch - 'a']++;
         }
 
@@ -15,25 +15,25 @@ class Solution {
         int[] freq = new int[26];
         int windowSize = 0;
         int n = p.length();
-        for(int right = 0; right < s.length(); right++) {
+        for (int right = 0; right < s.length(); right++) {
             freq[s.charAt(right) - 'a']++;
             windowSize++;
 
-            if(windowSize > n) {
+            if (windowSize > n) {
                 freq[s.charAt(left) - 'a']--;
                 left++;
                 windowSize--;
             }
 
-            if(windowSize == n) {
+            if (windowSize == n) {
                 boolean flag = true;
-                for(int i = 0; i < 26; i++) {
-                    if(freq[i] != freqP[i]) {
+                for (int i = 0; i < 26; i++) {
+                    if (freq[i] != freqP[i]) {
                         flag = false;
                     }
                 }
 
-                if(flag) {
+                if (flag) {
                     ans.add(left);
                 }
             }
