d4334d230816ffd9b8cef7877b4cf4d863ab0608
Rohan Figredo <rohanfigredo@gmail.com>
2026-09-12 16:09:08 +0530

    Time: 191 ms (12.96%), Space: 185.3 MB (42.59%) - LeetHub

--- 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.java
+++ 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.java
@@ -38,9 +38,59 @@ class Solution {
             nextIdx[i] = findNext(intervals, intervals.get(i).get(1));
         }
 
-        dp = new Pair[n][5];
+        dp = new Pair[n + 1][5];
 
-        Pair result = solve(intervals, 0, 4);
+        // Base case: k == 0
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = new Pair(0, new ArrayList<>());
+        }
+
+        // Base case: i>= n
+        for (int k = 0; k <= 4; k++) {
+            dp[n][k] = new Pair(0, new ArrayList<>());
+        }
+
+        for(int i = n - 1; i >= 0; i--) {
+            long weight = intervals.get(i).get(2);
+            int originalIndex = intervals.get(i).get(3);
+
+            for(int k = 1; k <= 4; k++) {
+                // Skip current interval
+                Pair skip = dp[i + 1][k];
+
+                // Take current interval
+                Pair next = dp[nextIdx[i]][k - 1];
+
+                List<Integer> takeIndexes = new ArrayList<>(next.indexes);
+                takeIndexes.add(originalIndex);
+
+                // Keep indexes sorted for lexicographical comparison
+                Collections.sort(takeIndexes);
+
+                Pair take = new Pair(
+                        weight + next.score,
+                        takeIndexes);
+
+                Pair result;
+
+                if (take.score > skip.score) {
+                    result = take;
+                } else if (take.score < skip.score) {
+                    result = skip;
+                } else {
+                    // Same weight -> lexicographically smaller indexes
+                    if (compare(take.indexes, skip.indexes) < 0) {
+                        result = take;
+                    } else {
+                        result = skip;
+                    }
+                }
+
+                dp[i][k] = result;
+            }
+        }
+
+        Pair result = dp[0][4];
 
         int[] ans = new int[result.indexes.size()];
 
@@ -51,52 +101,6 @@ class Solution {
         return ans;
     }
 
-    private Pair solve(List<List<Integer>> intervals, int i, int k) {
-
-        if (i >= n || k == 0) {
-            return new Pair(0, new ArrayList<>());
-        }
-
-        if (dp[i][k] != null) {
-            return dp[i][k];
-        }
-
-        // Skip current interval
-        Pair skip = solve(intervals, i + 1, k);
-
-        // Take current interval
-        Pair next = solve(intervals, nextIdx[i], k - 1);
-
-        long weight = intervals.get(i).get(2);
-        int originalIndex = intervals.get(i).get(3);
-
-        List<Integer> takeIndexes = new ArrayList<>(next.indexes);
-        takeIndexes.add(originalIndex);
-
-        // Keep indexes sorted for lexicographical comparison
-        Collections.sort(takeIndexes);
-
-        Pair take = new Pair(
-                weight + next.score,
-                takeIndexes);
-
-        Pair result;
-
-        if (take.score > skip.score) {
-            result = take;
-        } else if (take.score < skip.score) {
-            result = skip;
-        } else {
-            // Same weight -> lexicographically smaller indexes
-            if (compare(take.indexes, skip.indexes) < 0) {
-                result = take;
-            } else {
-                result = skip;
-            }
-        }
-
-        return dp[i][k] = result;
-    }
 
     private int findNext(List<List<Integer>> intervals, int endPoint) {
 
@@ -134,6 +138,142 @@ class Solution {
     }
 }
 
+// class Solution {
+
+//     class Pair {
+//         long score;
+//         List<Integer> indexes;
+
+//         Pair(long score, List<Integer> indexes) {
+//             this.score = score;
+//             this.indexes = indexes;
+//         }
+//     }
+
+//     int n;
+//     int[] nextIdx;
+//     Pair[][] dp;
+
+//     public int[] maximumWeight(List<List<Integer>> intervals) {
+//         n = intervals.size();
+
+//         // Add original index
+//         for (int i = 0; i < n; i++) {
+//             intervals.get(i).add(i);
+//         }
+
+//         // Sort by start time
+//         intervals.sort((a, b) -> {
+//             if (!a.get(0).equals(b.get(0))) {
+//                 return Integer.compare(a.get(0), b.get(0));
+//             }
+
+//             return Integer.compare(a.get(1), b.get(1));
+//         });
+
+//         // Precompute next non-overlapping interval
+//         nextIdx = new int[n];
+
+//         for (int i = 0; i < n; i++) {
+//             nextIdx[i] = findNext(intervals, intervals.get(i).get(1));
+//         }
+
+//         dp = new Pair[n][5];
+
+//         Pair result = solve(intervals, 0, 4);
+
+//         int[] ans = new int[result.indexes.size()];
+
+//         for (int i = 0; i < result.indexes.size(); i++) {
+//             ans[i] = result.indexes.get(i);
+//         }
+
+//         return ans;
+//     }
+
+//     private Pair solve(List<List<Integer>> intervals, int i, int k) {
+
+//         if (i >= n || k == 0) {
+//             return new Pair(0, new ArrayList<>());
+//         }
+
+//         if (dp[i][k] != null) {
+//             return dp[i][k];
+//         }
+
+//         // Skip current interval
+//         Pair skip = solve(intervals, i + 1, k);
+
+//         // Take current interval
+//         Pair next = solve(intervals, nextIdx[i], k - 1);
+
+//         long weight = intervals.get(i).get(2);
+//         int originalIndex = intervals.get(i).get(3);
+
+//         List<Integer> takeIndexes = new ArrayList<>(next.indexes);
+//         takeIndexes.add(originalIndex);
+
+//         // Keep indexes sorted for lexicographical comparison
+//         Collections.sort(takeIndexes);
+
+//         Pair take = new Pair(
+//                 weight + next.score,
+//                 takeIndexes);
+
+//         Pair result;
+
+//         if (take.score > skip.score) {
+//             result = take;
+//         } else if (take.score < skip.score) {
+//             result = skip;
+//         } else {
+//             // Same weight -> lexicographically smaller indexes
+//             if (compare(take.indexes, skip.indexes) < 0) {
+//                 result = take;
+//             } else {
+//                 result = skip;
+//             }
+//         }
+
+//         return dp[i][k] = result;
+//     }
+
+//     private int findNext(List<List<Integer>> intervals, int endPoint) {
+
+//         int low = 0;
+//         int high = n - 1;
+//         int res = n;
+
+//         while (low <= high) {
+
+//             int mid = low + (high - low) / 2;
+
+//             // Strictly non-overlapping
+//             if (intervals.get(mid).get(0) > endPoint) {
+//                 res = mid;
+//                 high = mid - 1;
+//             } else {
+//                 low = mid + 1;
+//             }
+//         }
+
+//         return res;
+//     }
+
+//     private int compare(List<Integer> a, List<Integer> b) {
+
+//         int size = Math.min(a.size(), b.size());
+
+//         for (int i = 0; i < size; i++) {
+//             if (!a.get(i).equals(b.get(i))) {
+//                 return Integer.compare(a.get(i), b.get(i));
+//             }
+//         }
+
+//         return Integer.compare(a.size(), b.size());
+//     }
+// }
+
 /*
     intervals = [[1,3,2],[4,5,2],[1,5,5],[6,9,3],[6,7,1],[8,9,1]]
     Consider we take interval 0, now we can take or not take next interval, and if we are taking next inteval then we must take non-overlapping line endCurr > startNext. But that leads to if we take current i as interval then we must traverse all intervals to take or not take instead of that we can sort based on starting point that gives us : 
@@ -147,7 +287,7 @@ class Solution {
 
     Steps :
      1. Intervals : store original indexes
-     2. Sort Intervals : sort(begin(intervals), end(intervals))
+     2. Sort Intervals : sort(begin(intervals), end(intervals)) O(nlon)
      3. Preprocess nextInterval(nextIdx) for each interval (so that we not use bs repeatedly, just use once)
      4. Solve(intervals, 0, k);
 
@@ -216,4 +356,31 @@ class Solution {
 
             return res;
         }
+
+    Bottom-Up:
+    dp[n + 1][k + 1];
+
+    for(int i = n - 1; i >= 0; i--) {  => O(n)
+        int wt = intervals[i][2];
+        int idx = intervals[i][3];
+        int j = nextIdx[i];
+        for(int k = 1; k <= 5; k++) {  => O(1)
+            skip = dp[i + 1][k];  // We need future values so i is reverse loop
+            temp = dp[j][k - 1];  // We need past values so normal k loop
+
+            Node take;
+            take.score = temp.score + wt;
+            take.indexes - temp.indexes;
+            take.indexes.add(idx);
+            sort(take.indexes from start to end);  take.size will be less than 4 => O(4log4) => O(1)
+
+            and if-else block same
+
+            dp[i][k] = res;
+        }
+    }
+
+    return dp[0][k].indexes;
+
+    Overall => O(nlogn) or k is genralized then it changes
 */
