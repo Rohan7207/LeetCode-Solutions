bd4ed3251930c724dcf1cab8e1b088c66b32c64b
Rohan Figredo <rohanfigredo@gmail.com>
2026-09-15 23:22:43 +0530

    Time: 22 ms (12.72%), Space: 48.1 MB (46.77%) - LeetHub

---
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.java
@@ -0,0 +1,64 @@
+class Solution {
+    public boolean validateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild) {
+        int root = findRoot(n, leftChild, rightChild);
+
+        if(root == -1) {
+            return false;
+        }
+
+        Stack<Integer> st = new Stack<>();
+        Set<Integer> seen = new HashSet<>();
+        st.push(root);
+        seen.add(root);
+
+        // DFS
+        while(!st.isEmpty()) {
+            int node = st.pop();
+            int[] children = new int[] {leftChild[node], rightChild[node]};
+
+            for(int child : children) {
+                if(child == -1) {
+                    continue;
+                }
+
+                if(seen.contains(child)) {
+                    return false;
+                }
+
+                st.push(child);
+                seen.add(child);
+            }
+        }
+
+        return seen.size() == n;
+    }
+
+    private int findRoot(int n, int[] left, int[] right) {
+        Set<Integer> children = new HashSet<>();
+
+        for(int node : left) {
+            children.add(node);
+        }
+
+        for(int node : right) {
+            children.add(node);
+        }
+
+        for(int i = 0; i < n; i++) {
+            if(!children.contains(i)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
+
+/*
+    Process in validating a binary tree:
+
+- If a binary tree does not have a root, then findRoot will return -1.
+- If there is a node with more than one parent, then we will detect it with seen.
+- If the tree is disconnected, then seen will hold less than n nodes at the end.
+- If there is a cycle, then we will detect it with seen.
+*/
