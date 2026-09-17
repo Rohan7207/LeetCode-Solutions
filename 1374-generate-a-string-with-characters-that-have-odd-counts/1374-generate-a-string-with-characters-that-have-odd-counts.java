9a6607343cd8aa329a40341f0783c1917c62aa4b
Rohan Figredo <rohanfigredo@gmail.com>
2026-09-17 18:59:01 +0530

    Refactor string generation to use character array
    Updated the implementation to use a character array for generating the string with odd character counts. Removed the commented-out StringBuilder approach.

--- 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.java
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.java
@@ -1,3 +1,20 @@
+// Problem: Generate a String With Characters That Have Odd Counts
+// Link: https://leetcode.com/problems/generate-a-string-with-characters-that-have-odd-counts/
+// Difficulty: Easy
+
+// Approach:
+// Use Character Frequency Construction.
+//
+// 1. Create a character array of length n and fill it with 'a'.
+// 2. If n is odd, all 'a' characters occur n times (odd).
+// 3. If n is even, change one character to 'b'.
+// 4. Now 'a' occurs n - 1 times (odd) and 'b' occurs once (odd).
+// 5. Return the character array as a String.
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+
 class Solution {
     public String generateTheString(int n) {
         char[] res = new char[n];
@@ -15,15 +32,4 @@ class Solution {
         return new String(res);
     }
 }
-
-/*
-    StringBuilder sb = new StringBuilder();
-
-    if(n % 2 != 0) {
-        sb.append("a".repeat(n));
-    } else {
-        sb.append("a".repeat(n - 1)).append("b");
-    }
-
-    return sb.toString();
 */
