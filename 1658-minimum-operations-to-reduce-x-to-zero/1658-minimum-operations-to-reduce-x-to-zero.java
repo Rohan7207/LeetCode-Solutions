5699bf66aecdb53e904d372636e101565d3034da
Rohan Figredo <rohanfigredo@gmail.com>
2026-09-23 16:13:33 +0530

    Time: 4 ms (98.07%), Space: 102.2 MB (44.57%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -3,22 +3,48 @@ class Solution {
         // We should find the maximum size subarray whose sum = total - x so that remaining elements is ans
         int n = nums.length;
         int total = 0;
-        int ans = Integer.MAX_VALUE;
 
         for (int num : nums) {
             total += num;
         }
 
         int target = total - x;
+        // If target is 0, we need to remove all elements
         if (target == 0) {
             return n;
         }
 
+        // If target is negative, impossible
         if (target < 0) {
             return -1;
         }
 
         int left = 0;
+        int currSum = 0;
+        int maxLength = -1;
+        for (int right = 0; right < n; right++) {
+            currSum += nums[right];
+
+            while (left < n && currSum > target) {
+                currSum -= nums[left];
+                left++;
+            }
+
+            if (currSum == target) {
+                maxLength = Math.max(maxLength, right - left + 1);
+            }
+        }
+
+        if(maxLength == -1) {
+            return -1;
+        }
+
+        return n - maxLength;
+    }
+}
+
+/*
+    int left = 0;
         int currSum = 0;
         int count = 0;
         for (int right = 0; right < n; right++) {
@@ -37,5 +63,4 @@ class Solution {
         }
 
         return ans == Integer.MAX_VALUE ? -1 : ans;
-    }
-}
+*/
