5bbae163d60cffacb2448ea5e83c3385dabea11b
Rohan Figredo <rohanfigredo@gmail.com>
2026-09-24 18:42:07 +0530

    Time: 9 ms (98.18%), Space: 46.3 MB (64.16%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.java
+++ 1390-four-divisors/1390-four-divisors.java
@@ -6,11 +6,17 @@ class Solution {
             int x = nums[i];
             int count = 0;
             int sum = 1 + nums[i];
+            int sqrt = (int) Math.sqrt(x);
 
-            for (int j = 2; j <= x / 2; j++) {
-                if (x % j == 0) {
+            for (int j = 2; j <= sqrt; j++) {
+                if(j * j == x) {
                     count++;
                     sum += j;
+                }
+
+                if (x % j == 0) {
+                    count += 2;
+                    sum += j + x / j;
                 }
 
                 if (count > 2) {
