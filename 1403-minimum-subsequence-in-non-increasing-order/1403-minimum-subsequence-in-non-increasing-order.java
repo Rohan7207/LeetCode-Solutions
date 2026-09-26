1a33d84f3c821082e8b8bacbb63a9edf3d8b5214
Rohan Figredo <rohanfigredo@gmail.com>
2026-09-26 15:01:46 +0530

    Time: 9 ms (32.58%), Space: 52.7 MB (8.61%) - LeetHub

---
+++ 1403-minimum-subsequence-in-non-increasing-order/1403-minimum-subsequence-in-non-increasing-order.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<Integer> minSubsequence(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+        int total = 0;
+
+        for(int num : nums) {
+            total += num;
+        }
+
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        int currSum = 0;
+
+        for(int i = n - 1; i >= 0; i--) {
+            currSum += nums[i];
+
+            if(currSum > total - currSum) {
+                ans.add(nums[i]);
+                break;
+            }
+
+            ans.add(nums[i]);
+        }
+
+        return ans;
+    }
+}
